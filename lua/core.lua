75a0d059a205752ae4b6746fac8d30dfe269b325
Dhruvin Shah <dhruvin3@gmail.com>
2021-07-28 10:35:08 -0400

    blankline removed redundant configs

--- lua/core.lua
+++ lua/core.lua
@@ -49,13 +49,6 @@ vim.g.maplocalleader = ' '
 vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
 vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
 
---Map blankline
-vim.g.indent_blankline_char = '┊'
-vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
-vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
-vim.g.indent_blankline_char_highlight = 'LineNr'
-vim.g.indent_blankline_show_trailing_blankline_indent = false
-
 -- Gitsigns
 require('gitsigns').setup {
   signs = {
