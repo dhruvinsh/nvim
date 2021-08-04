a8fd0da103b5ae16f8d016e1a82c8531ad532c45
Dhruvin Shah <dhruvin3@gmail.com>
2021-08-04 14:10:18 -0400

    few life of improvement autocmd

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -1,3 +1,25 @@
 -- Add spellchecking
 vim.cmd[[ autocmd FileType gitcommit setlocal spell ]]
 vim.cmd[[ autocmd FileType markdown setlocal spell ]]
+
+-- Highlight on yank
+vim.api.nvim_exec(
+  [[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+  augroup end
+]],
+  false
+)
+
+--Disable numbers in terminal mode
+vim.api.nvim_exec(
+  [[
+  augroup Terminal
+    autocmd!
+    au TermOpen * set nonu
+  augroup end
+]],
+  false
+)
