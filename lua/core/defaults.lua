5eefaabb45b73838ee6a90778d3d8cc7039efe34
Dhruvin Shah <dhruvin3@gmail.com>
2021-08-04 14:12:47 -0400

    few default settings ⚙

--- lua/core/defaults.lua
+++ lua/core/defaults.lua
@@ -6,3 +6,12 @@ local opt = vim.opt
 opt.relativenumber = true
 -- for better completion experience
 opt.completeopt = "menuone,noselect"
+-- save undo history
+opt.undofile = true
+-- enable break indent
+opt.breakindent = true
+-- Case insensitive searching UNLESS /C or capital in search
+opt.ignorecase = true
+opt.smartcase = true
+--Add map to enter paste mode
+opt.pastetoggle = '<F3>'
