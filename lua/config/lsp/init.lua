9f7044c3068271b91e6c7e0eecc0c8b120ed3cba
Dhruvin Shah <dhruvin3@gmail.com>
2021-08-05 21:52:11 -0400

    LSP: taking advantage of lspinstall 😎

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -1,6 +1,20 @@
--- CREDITS: defaults.nvim, folke/dot, LunarVim
+-- CREDITS: defaults.nvim, folke/dot, LunarVim, lspinstall
 local lspconfig = require("lspconfig")
 local lspinstall = require("lspinstall")
+
+
+-- TODO: some cool autocmd to look at
+-- Set autocommands conditional on server_capabilities
+-- if client.resolved_capabilities.document_highlight then
+--   vim.api.nvim_exec([[
+--   augroup lsp_document_highlight
+--   autocmd! * <buffer>
+--   autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+--   autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+--   augroup END
+--   ]], false)
+-- end
+
 
 -- keybindings
 local function add_lsp_buffer_keybindings(bufnr)
@@ -20,16 +34,27 @@ local function add_lsp_buffer_keybindings(bufnr)
     },
   }
   wk.register(keys, { mode = "n", buffer = bufnr })
+
+  -- TODO: something to look at later for conditional keybinding
+  -- Set some keybinds conditional on server capabilities
+  -- if client.resolved_capabilities.document_formatting then
+  --   buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  -- elseif client.resolved_capabilities.document_range_formatting then
+  --   buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+  -- end
+
 end
 
 -- enable on_attach
-local on_attach = function(_, bufnr)
+local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
   add_lsp_buffer_keybindings(bufnr)
 end
 
--- enable some capabilities
-local common_capabilities = function()
+-- pre-setup config maker
+-- config that activates keymaps and enables snippet support
+local function make_config()
+  -- enable some capabilities
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities.textDocument.completion.completionItem.snippetSupport = true
   capabilities.textDocument.completion.completionItem.resolveSupport = {
@@ -39,18 +64,35 @@ local common_capabilities = function()
       'additionalTextEdits',
     }
   }
-  return capabilities
+  return {
+    -- enable snippet support
+    capabilities = capabilities,
+    -- map buffer local keybindings when the language server attaches
+    on_attach = on_attach,
+  }
 end
 
--- setting up LSP
+-------------
+-- LSP setup
+-------------
 local function setup_servers()
   lspinstall.setup()
+
+  local custom_settings = require("config.lsp.settings")
   local servers = lspinstall.installed_servers()
+
   for _, server in pairs(servers) do
-    lspconfig[server].setup{
-      on_attach = on_attach,
-      capabilities = common_capabilities(),
-    }
+    -- NOTE to self: here server is string and in lua,
+    -- a dictionary is access via square brackets else
+    -- it can access via dot method.
+    local config = make_config()
+
+    -- some language specific setting override
+    if custom_settings[server] ~= nil then
+      config.settings = custom_settings[server]
+    end
+
+    lspconfig[server].setup(config)
 
     -- validation
     local cfg = lspconfig[server]
@@ -69,7 +111,9 @@ lspinstall.post_install_hook = function ()
 end
 
 
+--------------------------
 -- LSP management keymaps
+--------------------------
 vim.api.nvim_set_keymap('n', '<leader>lr', ':LspRestart<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>li', ':LspInfo<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>ls', ':LspStart<CR>', { noremap = true, silent = true })
