4a1326f26f8e44e3522a2fd6fea08deae04e4768
Dhruvin Shah <dhruvin3@gmail.com>
2021-08-12 11:12:34 -0400

    adjusting some keymaps ⌨

--- lua/config/gitsigns.lua
+++ lua/config/gitsigns.lua
@@ -26,22 +26,4 @@ require("gitsigns").setup({
       linehl = "GitSignsChangeLn",
     },
   },
-  keymaps = {
-    -- Default keymap options
-    noremap = true,
-    buffer = true,
-    ["n ]c"] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<cr>'" },
-    ["n [c"] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<cr>'" },
-    ["n <leader>ghs"] = '<cmd>lua require"gitsigns".stage_hunk()<cr>',
-    ['v <leader>ghs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<cr>',
-    ["n <leader>ghu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<cr>',
-    ["n <leader>ghr"] = '<cmd>lua require"gitsigns".reset_hunk()<cr>',
-    ['v <leader>ghr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<cr>',
-    ["n <leader>ghR"] = '<cmd>lua require"gitsigns".reset_buffer()<cr>',
-    ["n <leader>ghp"] = '<cmd>lua require"gitsigns".preview_hunk()<cr>',
-    ["n <leader>ghb"] = '<cmd>lua require"gitsigns".blame_line()<cr>',
-    -- Text objects
-    ["o ih"] = ':<C-U>lua require"gitsigns".select_hunk()<cr>',
-    ["x ih"] = ':<C-U>lua require"gitsigns".select_hunk()<cr>',
-  },
 })
--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,38 +6,66 @@ local keyopt1 = { noremap = true }
 local keyopt2 = { noremap = true, silent = true }
 local keyopt3 = { noremap = true, expr = true, silent = true }
 
+-------------------
 -- leader key
+-------------------
 setkey('', '<Space>', '<Nop>', keyopt2)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+------------------------
+-- Some custom keymaps
+------------------------
 -- Moving line bindings (I love this ones)
+setkey('i', '<A-j>', '<Esc>:m .+1<CR>==gi', keyopt1)
+setkey('i', '<A-k>', '<Esc>:m .-2<CR>==gi', keyopt1)
 setkey('n', '<A-j>', ':m .+1<CR>==', keyopt1)
 setkey('n', '<A-k>', ':m .-2<CR>==', keyopt1)
 setkey('v', '<A-j>', ":m '>+1<CR>gv=gv", keyopt1)
 setkey('v', '<A-k>', ":m '<-2<CR>gv=gv", keyopt1)
-setkey('i', '<A-j>', '<Esc>:m .+1<CR>==gi', keyopt1)
-setkey('i', '<A-k>', '<Esc>:m .-2<CR>==gi', keyopt1)
 --Remap for dealing with word wrap
 setkey('n', 'j', "v:count == 0 ? 'gj' : 'j'", keyopt3)
 setkey('n', 'k', "v:count == 0 ? 'gk' : 'k'", keyopt3)
 --Remap escape to leave terminal mode
 setkey('t', '<Esc>', [[<c-\><c-n>]], keyopt1)
 
+-------------------
 -- Buffer keymaps
+-------------------
 setkey('n', '<leader>bd', ':bdelete<CR>', keyopt2)
 setkey("n", "<leader>bs", "<cmd>lua require('telescope.builtin').buffers()<cr>", keyopt2)
 
+-------------------
 -- Files keymaps
+-------------------
 setkey("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", keyopt2)
 setkey("n", "<leader>fr", "<cmd>lua require('telescope.builtin').oldfiles()<cr>", keyopt2)
 setkey("n", "<leader>ft", "<cmd>NvimTreeToggle<cr>", keyopt2)
 setkey("n", "<leader>fz", "<cmd>ZenMode<cr>", keyopt2)
 
+-------------------
 -- Git keymaps
+-------------------
+setkey("n", "<leader>ghP", "<cmd>lua require('gitsigns').preview_hunk()<cr>", keyopt2)
+setkey("n", "<leader>ghR", "<cmd>lua require('gitsigns').reset_buffer()<cr>", keyopt2)
 setkey("n", "<leader>gf", "<cmd>lua require('telescope.builtin').git_files()<cr>", keyopt2)
+setkey("n", "<leader>ghb", "<cmd>lua require('gitsigns').blame_line()<cr>", keyopt2)
+setkey("n", "<leader>ghn", "<cmd>lua require('gitsigns').next_hunk()<cr>", keyopt2)
+setkey("n", "<leader>ghp", "<cmd>lua require('gitsigns').prev_hunk()<cr>", keyopt2)
+setkey("n", "<leader>ghr", "<cmd>lua require('gitsigns').reset_hunk()<cr>", keyopt2)
+setkey("n", "<leader>ghs", "<cmd>lua require('gitsigns').stage_hunk()<cr>", keyopt2)
+setkey("n", "<leader>ghu", "<cmd>lua require('gitsigns').undo_stage_hunk()<cr>", keyopt2)
 
+setkey("o", "ih", ":<C-U>lua require('gitsigns').select_hunk()<cr>", keyopt2)
+
+setkey("v", "<leader>ghr", "<cmd>lua require('gitsigns').reset_hunk({vim.fn.line('.'), vim.fn.line('v')})<cr>", keyopt2)
+setkey("v", "<leader>ghs", "<cmd>lua require('gitsigns').stage_hunk({vim.fn.line('.'), vim.fn.line('v')})<cr>", keyopt2)
+
+setkey("x", "ih", ":<C-U>lua require('gitsigns').select_hunk()<cr>", keyopt2)
+
+-------------------
 -- Search keymaps
+-------------------
 setkey("n", "<leader>sC", "<cmd>lua require('telescope.builtin').colorscheme()<cr>", keyopt2)
 setkey("n", "<leader>sc", "<cmd>lua require('telescope.builtin').commands()<cr>", keyopt2)
 -- some how i keep pressing this to open files, probably from default.nvim
