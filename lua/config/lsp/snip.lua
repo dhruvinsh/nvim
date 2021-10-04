9b1eb495a5784847bdfd95e600bb6b026cd6e297
Dhruvin Shah <dshah@evertz.com>
2021-10-04 12:07:41 -0400

    lsp: luasnipe config change

--- lua/config/lsp/snip.lua
+++ lua/config/lsp/snip.lua
@@ -1,7 +1,5 @@
 local luasnip = require("luasnip")
 luasnip.config.set_config({
   history = true,
-  -- Update more often, :h events for more info.
-  updateevents = "TextChanged,TextChangedI",
 })
 require("luasnip/loaders/from_vscode").load()
