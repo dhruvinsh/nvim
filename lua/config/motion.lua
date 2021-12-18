22628d2baf0b52f2a1b719934d125f35e8062c7e
Dhruvin Shah <dhruvin3@gmail.com>
2021-12-18 15:01:51 -0500

    fixed lightspeed recent update

--- lua/config/motion.lua
+++ lua/config/motion.lua
@@ -1,5 +1,4 @@
 require("lightspeed").setup({
-  jump_to_first_match = true,
   jump_on_partial_input_safety_timeout = 400,
   -- This can get _really_ slow if the window has a lot of content,
   -- turn it on only if your machine can always cope with it.
