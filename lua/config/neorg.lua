c677dbabb1ecd680386ecfbd0663e20e8247a9c9
Dhruvin Shah <dhruvin3@gmail.com>
2022-03-12 09:46:07 -0500

    neorg adding nvim-cmp as completion

--- lua/config/neorg.lua
+++ lua/config/neorg.lua
@@ -4,7 +4,12 @@ require("neorg").setup({
   load = {
     ["core.defaults"] = {},
     ["core.norg.concealer"] = {},
-    ["core.norg.completion"] = {},
+    ["core.norg.completion"] = {
+      config = {
+        engine = "nvim-cmp",
+      },
+    },
+    ["core.integrations.nvim-cmp"] = {},
     ["core.norg.dirman"] = {
       config = {
         workspaces = {
