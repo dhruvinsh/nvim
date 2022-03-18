2e59f69ae0f25204ffb2a775c53ec3de45a50e0c
Dhruvin Shah <dhruvin3@gmail.com>
2022-03-18 15:15:25 -0400

    config: rainbow plugin fixed with change in onedark

--- lua/config/rainbow.lua
+++ lua/config/rainbow.lua
@@ -1,4 +1,4 @@
-local c = require("onedark.colors")
+local c = require("theme.onedark")
 
 require("nvim-treesitter.configs").setup({
   rainbow = {
@@ -6,7 +6,7 @@ require("nvim-treesitter.configs").setup({
     extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
     max_file_lines = nil, -- Do not enable for files with more than n lines, int
     colors = {
-      c.bg_blue,
+      c.nord_blue,
       c.purple,
       c.yellow,
       c.cyan,
