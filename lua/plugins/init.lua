3477b0bdb7195cbff06a147f9cd48d145a328c5b
Dhruvin Shah <dshah@evertz.com>
2022-03-18 14:54:25 -0400

    plugin: telescope fzf plugin added

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -33,7 +33,13 @@ return require("packer").startup(function(use)
   -- file manager
   use({
     "nvim-telescope/telescope.nvim",
-    requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } },
+    requires = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        run = "make",
+      },
+    },
     config = [[ require('config.telescope') ]],
   })
   -- icons
