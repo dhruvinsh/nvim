46c14ca3ca34fb7008d4e519b25ca5e95db40f11
Dhruvin Shah <dhruvin3@gmail.com>
2022-03-24 12:59:19 -0400

    plugin: impatient added
    * refactoring main init modules

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
--- where it all starts..
-require("core")
-require("plugins")
+local present, impatient = pcall(require, "impatient")
+
+if present then
+   impatient.enable_profile()
+end
+
+local core_modules = {
+  "core",
+  "plugins",
+}
+
+for _, m in ipairs(core_modules) do
+  local ok, err = pcall(require, m)
+  if not ok then
+    error("Error loading" .. m .."\n\n" .. err)
+  end
+end
