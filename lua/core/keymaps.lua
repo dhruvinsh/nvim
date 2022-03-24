03711654cbcea4b67d93dbe6b483fbbde1f203d8
Dhruvin Shah <dhruvin3@gmail.com>
2022-03-23 22:15:38 -0400

    added missing keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -55,6 +55,7 @@ setkey("n", "<leader>dt", ":lua require('neogen').generate({type='type'})<cr>", 
 setkey("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", keyopt2)
 setkey("n", "<leader>fr", "<cmd>lua require('telescope.builtin').oldfiles()<cr>", keyopt2)
 setkey("n", "<leader>ft", "<cmd>NvimTreeToggle<cr>", keyopt2)
+setkey("n", "<leader>fT", "<cmd>ToggleTerm<cr>", keyopt2)
 setkey("n", "<leader>fz", "<cmd>TZAtaraxis<cr>", keyopt2)
 
 -------------------
