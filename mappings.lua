e441844f0368e68969ff0c2f68e24531d9365aef
Dhruvin Shah <dshah@evertz.com>
2022-09-09 11:29:27 -0400

    terminal keymaps added

--- mappings.lua
+++ mappings.lua
@@ -1,9 +1,16 @@
 local M = {}
 
 M.nvimtree = {
-    n = {
-        ["<leader>ft"] = { "<cmd> NvimTreeToggle <CR>", "toggle nvimtree" }
-    }
+  n = {
+      ["<leader>ft"] = { "<cmd> NvimTreeToggle <CR>", "toggle nvimtree" },
+  },
+  t = {
+    ["<Esc>"] = { [[<C-\><C-n>]] },
+    ["<C-w>h"] = { [[<C-\><C-n><C-W>h]] },
+    ["<C-w>j"] = { [[<C-\><C-n><C-W>j]] },
+    ["<C-w>k"] = { [[<C-\><C-n><C-W>k]] },
+    ["<C-w>l"] = { [[<C-\><C-n><C-W>l]] },
+  },
 }
 
 return M
