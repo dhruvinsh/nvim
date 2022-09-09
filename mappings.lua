31c95d6a519dc3191f0df2d3b66ffab92b4af8aa
Dhruvin Shah <dhruvin3@gmail.com>
2022-09-09 14:01:40 -0400

    keympas update

--- mappings.lua
+++ mappings.lua
@@ -1,12 +1,38 @@
 local M = {}
 
+-- global disable keymaps
+M.disable = {
+  n = {
+    ["<leader>b"] = "", -- unmap create buffer
+  }
+}
+
+-- nvimtree keymaps
 M.nvimtree = {
   n = {
     ["<leader>ft"] = { "<cmd> NvimTreeToggle <CR>", "toggle nvimtree" },
-
-    -- git keymaps
-    ["<leader>gg"] = { "<cmd>Neogit <CR>", "Neogit"}
   },
+}
+
+-- git keymaps
+M.neogit = {
+  n = {
+    -- git keymaps
+    ["<leader>gg"] = { "<cmd>Neogit <CR>", "Neogit"},
+  },
+}
+
+-- buffer managment
+M.buffer = {
+  n = {
+    ["<leader>bb"] = { "", "buffers" },
+    ["<leader>bc"] = { "<cmd> enew <CR>", "new buffer" },
+    ["<leader>bd"] = { [[ rquire("core.utils").close_buffer() ]], "close buffer" },
+  },
+}
+
+-- terminal managment
+M.terminal = {
   t = {
     ["<Esc>"] = { [[<C-\><C-n>]] },
     ["<C-w>h"] = { [[<C-\><C-n><C-W>h]] },
