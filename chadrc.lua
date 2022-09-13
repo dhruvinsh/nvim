6e0a8d4869492886fd4b8aa2d0cea51e80c98559
Dhruvin Shah <dhruvin3@gmail.com>
2022-09-13 00:50:41 +0000

    nvchad moving to new plugin configs

--- chadrc.lua
+++ chadrc.lua
@@ -1,14 +1,50 @@
 -- Just an example, supposed to be placed in /lua/custom/
 
 local M = {}
-local pluginConfs = require "custom.plugins.configs"
 
 -- make sure you maintain the structure of `core/default_config.lua` here,
 M.plugins = {
-  override = {
-    ["williamboman/mason.nvim"] = pluginConfs.mason,
+  ["folke/which-key.nvim"] = {
+      disable = false,
   },
-  user = require "custom.plugins",
+  ["max397574/better-escape.nvim"] = {
+      event = "InsertEnter",
+      config = function()
+          require("better_escape").setup()
+      end,
+  },
+  ["TimUntersberger/neogit"] = {
+      cmd = require("custom.lazy_load").neogit_cmds,
+      requires = "nvim-lua/plenary.nvim",
+  },
+  ["jose-elias-alvarez/null-ls.nvim"] = {
+    after = "nvim-lspconfig",
+    config = function()
+       require "custom.plugins.null-ls"
+    end,
+  },
+  ["williamboman/mason.nvim"] = {
+    override_options = {
+      ensure_installed = {
+        -- lua stuff
+        "lua-language-server",
+        "stylua",
+
+        -- python
+        "pyright",
+
+        -- web dev
+        "css-lsp",
+        "html-lsp",
+        "typescript-language-server",
+        "json-lsp",
+
+        -- shell
+        "shfmt",
+        "shellcheck",
+      },
+    },
+  },
 }
 
 M.mappings = require "custom.mappings"
