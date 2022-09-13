2c5b28a092c5e471727eda3cb79df5fd75fc9e22
Dhruvin Shah <dshah@evertz.com>
2022-09-13 09:29:50 -0400

    better config

--- chadrc.lua
+++ chadrc.lua
@@ -1,58 +1,12 @@
--- Just an example, supposed to be placed in /lua/custom/
-
 local M = {}
 
--- make sure you maintain the structure of `core/default_config.lua` here,
-M.plugins = {
-  ["folke/which-key.nvim"] = {
-      disable = false,
-  },
-  ["max397574/better-escape.nvim"] = {
-      event = "InsertEnter",
-      config = function()
-          require("better_escape").setup()
-      end,
-  },
-  ["TimUntersberger/neogit"] = {
-      cmd = require("custom.lazy_load").neogit_cmds,
-      requires = "nvim-lua/plenary.nvim",
-  },
-  ["jose-elias-alvarez/null-ls.nvim"] = {
-    after = "nvim-lspconfig",
-    config = function()
-       require "custom.plugins.null-ls"
-    end,
-  },
-  ["williamboman/mason.nvim"] = {
-    override_options = {
-      ensure_installed = {
-        -- lua stuff
-        "lua-language-server",
-        "stylua",
-
-        -- python
-        "pyright",
-
-        -- web dev
-        "css-lsp",
-        "html-lsp",
-        "typescript-language-server",
-        "json-lsp",
-
-        -- shell
-        "shfmt",
-        "shellcheck",
-      },
-    },
-  },
-  ["neovim/nvim-lspconfig"] = {
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.plugins.lspconfig"
-    end,
-  },
+M.ui = {
+  theme_toggle = { "onedark", "one_light" },
+  theme = "onedark",
 }
 
-M.mappings = require "custom.mappings"
+M.plugins = require("custom.plugins")
+
+M.mappings = require("custom.mappings")
 
 return M
--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "pyright" }
+local servers = { "pyright", "html", "cssls", "tsserver", "clangd" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
