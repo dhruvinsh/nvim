4d66dcd631e79de595257e94af1ff5c7cbc16c7c
Dhruvin Shah <dshah@evertz.com>
2022-09-15 11:31:46 -0400

    autocmd to create missing directory when saving
    * credits: https://jdhao.github.io/2022/08/21/you-do-not-need-a-plugin-for-this/

--- init.lua
+++ init.lua
@@ -1,3 +1,15 @@
 -- example file i.e lua/custom/init.lua
 -- load your options globals, autocmds here or anything .__.
 -- you can even override default options here (core/options.lua)
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("BufWritePre", {
+  pattern = "*",
+  group = augroup("auto_create_dir", { clear = true }),
+  callback = function(ctx)
+    local dir = vim.fn.fnamemodify(ctx.file, ":p:h")
+    vim.fn.mkdir(dir, "p")
+  end,
+})
