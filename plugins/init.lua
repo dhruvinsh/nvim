e8441f223cadcfecda6e51ad794b13fd06616498
Dhruvin Shah <dhruvin3@gmail.com>
2022-09-17 19:07:39 +0000

    orgmode support added!

--- plugins/init.lua
+++ plugins/init.lua
@@ -77,4 +77,16 @@ return {
       require("custom.plugins.null-ls")
     end,
   },
+
+  -- languages
+  ["nvim-orgmode/orgmode"] = {
+    ft = "org",
+    config = function()
+      require("orgmode").setup_ts_grammar()
+      require("orgmode").setup({
+        org_agenda_files = { "~/Documents/org/*" },
+        org_default_notes_file = "~/Documents/org/refile.org",
+      })
+    end,
+  },
 }
