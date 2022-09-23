f0509ab227ec4ffa3a7d4465cd94fdc20d648515
Dhruvin Shah <dhruvin3@gmail.com>
2022-09-23 14:42:59 -0400

    plugin(new): nvim-ufo for folding added

--- init.lua
+++ init.lua
@@ -67,6 +67,34 @@ require("packer").startup({
       tag = "*",
       config = [[ require("nvim-surround").setup() ]],
     })
+    use({
+      "kevinhwang91/nvim-ufo",
+      requires = "kevinhwang91/promise-async",
+      config = function()
+        local ufo = require("ufo")
+        vim.keymap.set('n', 'zR', ufo.openAllFolds)
+        vim.keymap.set('n', 'zM', ufo.closeAllFolds)
+        vim.keymap.set('n', 'zr', ufo.openFoldsExceptKinds)
+        vim.keymap.set('n', 'zm', ufo.closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+        vim.keymap.set('n', 'K', function()
+    local winid = require('ufo').peekFoldedLinesUnderCursor()
+    if not winid then
+        vim.lsp.buf.hover()
+    end
+end)
+        ufo.setup({
+          provider_selector = function(bufnr, filetype, buftype)
+            return { "treesitter", "indent" }
+          end,
+        })
+      end,
+      setup = function()
+        vim.o.foldcolumn = "1"
+        vim.o.foldlevel = 99
+        vim.o.foldlevelstart = 99
+        vim.o.foldenable = true
+      end,
+    })
 
     -- Movement
     use({
