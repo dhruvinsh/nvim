e609e9217a56a16a9a69ca7449439d529a03eecf
Dhruvin Shah <dhruvin3@gmail.com>
2022-09-26 17:20:35 -0400

    plugin(new): todo-comments added for highlights

--- init.lua
+++ init.lua
@@ -145,7 +145,15 @@ require("packer").startup({
     use("ellisonleao/gruvbox.nvim")
 
     use("nvim-lualine/lualine.nvim") -- Fancier statusline
+
+    -- Highlights
     use("lukas-reineke/indent-blankline.nvim") -- Add indentation guides even on blank lines
+    use({
+      "folke/todo-comments.nvim",
+      requires = "nvim-lua/plenary.nvim",
+      config = [[require("todo-comments").setup() ]],
+    })
+
     use("tpope/vim-sleuth") -- Detect tabstop and shiftwidth automatically
 
     -- Fuzzy Finder (files, lsp, etc)
