0f37303901c1eaad4a23b8ec4ebd8fd9feb5ff07
Dhruvin Shah <dshah@evertz.com>
2022-09-27 10:39:07 -0400

    code(project): keymap comments added

--- init.lua
+++ init.lua
@@ -254,6 +254,15 @@ require("packer").startup({
     })
 
     -- project
+    -- default keymapping:
+    -- | Normal mode | Insert mode | Action                     |
+    -- | ----------- | ----------- | -------------------------- |
+    -- | f           | <c-f>       | find_project_files         |
+    -- | b           | <c-b>       | browse_project_files       |
+    -- | d           | <c-d>       | delete_project             |
+    -- | s           | <c-s>       | search_in_project_files    |
+    -- | r           | <c-r>       | recent_project_files       |
+    -- | w           | <c-w>       | change_working_directory   |
     use({
       "ahmedkhalf/project.nvim",
       config = function()
