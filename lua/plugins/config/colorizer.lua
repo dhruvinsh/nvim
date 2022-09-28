1298efd01cf93c0f5cf4bb3311c4ef1f92ae6b0f
Dhruvin Shah <dhruvin3@gmail.com>
2022-09-28 16:19:02 -0400

    lint(stylua): colomun length to 120

--- lua/plugins/config/colorizer.lua
+++ lua/plugins/config/colorizer.lua
@@ -3,4 +3,3 @@ local status_ok, colorizer = pcall(require, "colorizer")
 if status_ok then
   colorizer.setup()
 end
-
--- lua/plugins/config/neogen.lua
+++ lua/plugins/config/neogen.lua
@@ -3,4 +3,3 @@ local status_ok, neogen = pcall(require, "neogen")
 if status_ok then
   neogen.setup({})
 end
-
