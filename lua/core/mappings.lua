b3174ea8157c7cdbf26e4a2ac20cda9dcec5a17c
Dhruvin Shah <dhruvin3@gmail.com>
2022-09-28 22:49:27 +0000

    mapping(core): using kmap for setting mapping

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -7,8 +7,8 @@ local kmap = vim.keymap.set
 g.mapleader = " "
 g.maplocalleader = " "
 
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+kmap({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+kmap("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+kmap("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
