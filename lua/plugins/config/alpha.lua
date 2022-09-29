43e51f8b05e487c8503e7c4f0cdf9cc4ef5ea74c
Dhruvin Shah <dhruvin3@gmail.com>
2022-09-29 02:47:49 +0000

    plugin(alpha): enabled and configured

--- lua/plugins/config/alpha.lua
+++ lua/plugins/config/alpha.lua
@@ -1,7 +1,33 @@
 local status_ok, alpha = pcall(require, "alpha")
-
-if status_ok then
-  alpha.setup({
-    require("alpha.themes.dashboard").config,
-  })
+if not status_ok then
+  return
 end
+
+local dashboard = require("alpha.themes.dashboard")
+
+dashboard.section.header.val = {
+  [[                               __                ]],
+  [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+  [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+  [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+  [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+  [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+}
+dashboard.section.buttons.val = {
+  dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+  dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+  dashboard.button("p", "  Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
+  dashboard.button("r", "  Recent files", ":Telescope oldfiles <CR>"),
+  dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+  dashboard.button("s", "  Find Session", "<cmd>SessionManager load_current_dir_session<CR>"),
+  dashboard.button("c", "  Config", ":e ~/.config/nvim/init.lua <CR>"),
+  dashboard.button("u", "  Update", ":PackerSync<CR>"),
+  dashboard.button("q", "  Quit", ":qa<CR>"),
+}
+local function footer()
+  return ""
+end
+
+dashboard.section.footer.val = footer()
+dashboard.opts.opts.noautocmd = true
+alpha.setup(dashboard.opts)
