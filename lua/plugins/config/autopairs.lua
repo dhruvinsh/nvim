9da0b38853658776bbe98555429f1128135f0e49
Dhruvin Shah <dhruvin3@gmail.com>
2022-09-29 15:03:39 +0000

    plugin(autopairs): refactoring code to its own config

--- lua/plugins/config/autopairs.lua
+++ lua/plugins/config/autopairs.lua
@@ -1,5 +1,14 @@
 local status_ok, autopairs = pcall(require, "nvim-autopairs")
 
-if status_ok then
-  autopairs.setup({})
+if not status_ok then
+  return
 end
+autopairs.setup({})
+
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+  return
+end
+
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
--- lua/plugins/config/lsp.lua
+++ lua/plugins/config/lsp.lua
@@ -172,7 +172,3 @@ cmp.setup({
 
 -- luasnipe config
 require("luasnip.loaders.from_vscode").lazy_load()
-
--- nvim-autopairs and nvim-cmp <CR> mapping
-local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
