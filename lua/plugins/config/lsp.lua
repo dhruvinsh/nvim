bb8e6e84fb000f2c3f031d999c1bc7e5eea57f7b
Dhruvin Shah <dhruvin3@gmail.com>
2022-10-01 04:05:24 +0000

    plugin(lsp): cleaning up few more extra code

--- lua/plugins/config/lsp.lua
+++ lua/plugins/config/lsp.lua
@@ -14,12 +14,6 @@ end
 -- LSP settings.
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
-  -- NOTE: Remember that lua is a real programming language, and as such it is possible
-  -- to define small helper and utility functions so you don't have to repeat yourself
-  -- many times.
-  --
-  -- In this case, we create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
   local nmap = function(keys, func, desc)
     if desc then
       desc = "LSP: " .. desc
@@ -52,11 +46,8 @@ local on_attach = function(_, bufnr)
 
   -- Create a command `:Format` local to the LSP buffer and add keymaps
   vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-    if vim.lsp.buf.format then
-      vim.lsp.buf.format({ async = true })
-    elseif vim.lsp.buf.formatting then
-      vim.lsp.buf.formatting()
-    end
+    -- vim.lsp.buf.formatting is deprecated now
+    vim.lsp.buf.format({ async = true })
   end, { desc = "Format current buffer with LSP" })
 end
 vim.keymap.set("n", "<leader>bf", "<cmd>Format <CR>", { desc = "[F]ormat" })
@@ -102,31 +93,3 @@ for _, lsp in ipairs(servers) do
     })
   end
 end
-
--- local runtime_path = vim.split(package.path, ";")
--- table.insert(runtime_path, "lua/?.lua")
--- table.insert(runtime_path, "lua/?/init.lua")
---
--- require("lspconfig").sumneko_lua.setup({
---   on_attach = on_attach,
---   capabilities = capabilities,
---   settings = {
---     Lua = {
---       runtime = {
---         -- Tell the language server which version of Lua you're using (most likely LuaJIT)
---         version = "LuaJIT",
---         -- Setup your lua path
---         path = runtime_path,
---       },
---       diagnostics = {
---         globals = { "vim" },
---       },
---       workspace = { library = vim.api.nvim_get_runtime_file("", true) },
---       -- Do not send telemetry data containing a randomized but unique identifier
---       telemetry = { enable = false },
---       completion = {
---         callSnippet = "Replace",
---       },
---     },
---   },
--- })
