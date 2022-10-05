2e2183cdb6a7d5575fd7ee024983ccdf5890b1b5
Dhruvin Shah <dhruvin3@gmail.com>
2022-10-05 03:15:14 +0000

    config(aerial): settings added with legendary keymaps

--- lua/plugins/config/aerial.lua
+++ lua/plugins/config/aerial.lua
@@ -1,8 +1,31 @@
 local status_ok, aerial = pcall(require, "aerial")
 
-if status_ok then
-  aerial.setup()
-
-  -- if telescope loaded then load the aerial plugin as well.
-  pcall(require("telescope").load_extension, "aerial")
+if not status_ok then
+  return
 end
+
+local legendary_status_ok, legendary = pcall(require, "legendary")
+
+if not legendary_status_ok then
+  return
+end
+
+aerial.setup({
+  attach_mode = "global",
+  backends = { "lsp", "treesitter" },
+  min_width = 28,
+  show_guides = true,
+  filter_kind = false,
+  guides = {
+    mid_item = "├ ",
+    last_item = "└ ",
+    nested_top = "│ ",
+    whitespace = "  ",
+  },
+  on_attach = function(bufnr)
+    legendary.bind_keymaps(require("plugins.config.legendary").aerial(bufnr))
+  end,
+})
+
+-- if telescope loaded then load the aerial plugin as well.
+pcall(require("telescope").load_extension, "aerial")
