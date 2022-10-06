be8b0c5c253ab2bf3d2b33c534ec04a5a47b77b0
Dhruvin Shah <dhruvin3@gmail.com>
2022-10-06 04:15:53 +0000

    keymaps(aerial): moved to lsp for now

--- lua/plugins/config/lsp.lua
+++ lua/plugins/config/lsp.lua
@@ -22,6 +22,12 @@ local on_attach = function(_, bufnr)
     vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
   end
 
+  -- aerial keymaps
+  nmap("{", ":AerialPrev <CR>", "Jump backwards in Aerial")
+  nmap("}", ":AerialNext <CR>", "Jump forwards in Aerial")
+  nmap("[[", ":AerialPrevUp <CR>", "Jump up and backwards in Aerial")
+  nmap("]]", ":AerialNextUp <CR>", "Jump up and forward in Aerial")
+
   nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
   nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
