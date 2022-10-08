2d2622648c6dabd920fd80711cbffba9ca1aa701
Dhruvin Shah <dhruvin3@gmail.com>
2022-10-08 18:15:58 +0000

    core(autocmd): hide bufferline dynamically

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -16,3 +16,21 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.opt_local.spell = true
   end,
 })
+
+-- Hide bufferline if dashboard is open
+local toggle_bufferline = vim.api.nvim_create_augroup("ToggleBufferline", { clear = true })
+vim.api.nvim_create_autocmd("User", {
+  callback = function()
+    vim.o.showtabline = 0
+  end,
+  group = toggle_bufferline,
+  pattern = "AlphaReady",
+})
+vim.api.nvim_create_autocmd("BufUnload", {
+  callback = function()
+    print("running this..")
+    vim.o.showtabline = 2
+  end,
+  group = toggle_bufferline,
+  pattern = "<buffer>",
+})
