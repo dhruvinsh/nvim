3e4cbb21872315abab17e85de5a0c9fe7fd28520
Dhruvin Shah <dhruvin3@gmail.com>
2022-10-08 03:35:06 +0000

    config(keymap): which-key opt doc added

--- lua/plugins/config/mappings.lua
+++ lua/plugins/config/mappings.lua
@@ -1,5 +1,16 @@
 local M = {}
 
+-- NOTE: keymaps apllied via which-key have default options as below
+-- {
+--   mode = "n", -- NORMAL mode
+--   -- prefix: use "<leader>f" for example for mapping everything related to finding files
+--   -- the prefix is prepended to every mapping part of `mappings`
+--   prefix = "",
+--   buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+--   silent = true, -- use `silent` when creating keymaps
+--   noremap = true, -- use `noremap` when creating keymaps
+--   nowait = false, -- use `nowait` when creating keymaps
+-- }
 
 -- Labels for the whichkey
 M.tags = {
