c7bc0903152a1ad0705a6a84d8bdebfb9e13dec7
Dhruvin Shah <dhruvin3@gmail.com>
2022-10-08 03:02:11 +0000

    config(fidget): null-ls source disable

--- lua/plugins/config/fidget.lua
+++ lua/plugins/config/fidget.lua
@@ -39,6 +39,10 @@ fidget.setup({
       return string.format("%s%s [%s]", message, percentage and string.format(" (%s%%)", percentage) or "", task_name)
     end,
   },
+  sources = {
+    -- null-ls fortmat makes lot of notifications
+    ["null-ls"] = { ignore = true },
+  },
   debug = {
     logging = false, -- whether to enable logging, for debugging
     strict = false, -- whether to interpret LSP strictly
