97da328a7447d2af5899e084ec8af16f0387e9e9
Dhruvin Shah <dhruvin3@gmail.com>
2022-10-14 04:40:36 +0000

    keymaps(lsp): moved to core

--- lua/orion/plugins/config/lsp.lua
+++ lua/orion/plugins/config/lsp.lua
@@ -14,49 +14,13 @@ end
 -- LSP settings.
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = "LSP: " .. desc
-    end
+  local keymaps = require("orion.core.mappings")
+  local util = require("orion.core.util")
 
-    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-  end
+  local mappings = keymaps.lsp(bufnr)
 
-  -- aerial keymaps
-  nmap("{", ":AerialPrev <CR>", "Jump backwards in Aerial")
-  nmap("}", ":AerialNext <CR>", "Jump forwards in Aerial")
-  nmap("[[", ":AerialPrevUp <CR>", "Jump up and backwards in Aerial")
-  nmap("]]", ":AerialNextUp <CR>", "Jump up and forward in Aerial")
-
-  nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-  nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
-  nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-  nmap("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-  nmap("gr", require("telescope.builtin").lsp_references)
-  nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-  nmap("<leader><Tab>s", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-  -- See `:help K` for why this keymap
-  nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-  nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-
-  -- Lesser used LSP functionality
-  nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-  nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-  nmap("<leader><Tab>a", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-  nmap("<leader><Tab>r", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-  nmap("<leader><Tab>l", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, "[W]orkspace [L]ist Folders")
-
-  -- Create a command `:Format` local to the LSP buffer and add keymaps
-  vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-    -- vim.lsp.buf.formatting is deprecated now
-    vim.lsp.buf.format({ async = true })
-  end, { desc = "Format current buffer with LSP" })
+  util.setup_keymap(mappings)
 end
-vim.keymap.set("n", "<leader>bf", "<cmd>Format <CR>", { desc = "[F]ormat" })
 
 -- nvim-cmp supports additional completion capabilities
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
