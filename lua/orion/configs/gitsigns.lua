702a94a5e7e762a565db922f35e3c47b25057779
Dhruvin Shah <dhruvin3@gmail.com>
2022-10-16 01:59:59 +0000

    plugin(gitsigns): some better visuals

--- lua/orion/configs/gitsigns.lua
+++ lua/orion/configs/gitsigns.lua
@@ -3,11 +3,11 @@ local status_ok, gitsigns = pcall(require, "gitsigns")
 if status_ok then
   gitsigns.setup({
     signs = {
-      add = { text = "+" },
-      change = { text = "~" },
-      delete = { text = "_" },
-      topdelete = { text = "‾" },
-      changedelete = { text = "~" },
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "▎" },
+      topdelete = { text = "契" },
+      changedelete = { text = "▎" },
     },
   })
 end
