ad9d9ff5b7d61b154ff19c73accf8076c21c4937
Dhruvin Shah <dhruvin3@gmail.com>
2022-10-22 16:47:44 -0400

    plugin(new): git-conflict added

---
+++ lua/orion/configs/git-conflict.lua
@@ -0,0 +1,14 @@
+local status_ok, git_conflict = pcall(require, "git-conflict")
+
+if not status_ok then
+  return
+end
+
+git_conflict.setup({
+  default_mappings = true, -- disable buffer local mapping created by this plugin
+  disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
+  highlights = { -- They must have background color, otherwise the default color will be used
+    incoming = "DiffText",
+    current = "DiffAdd",
+  },
+})
