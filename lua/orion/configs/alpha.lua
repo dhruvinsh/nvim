5515148c8413975c48fa817adf7bc822ac3bc351
Dhruvin Shah <dhruvin3@gmail.com>
2022-10-25 20:37:30 -0400

    keymaps(core): telescope find_files shows hidden now

--- lua/orion/configs/alpha.lua
+++ lua/orion/configs/alpha.lua
@@ -15,7 +15,7 @@ dashboard.section.header.val = {
 }
 
 dashboard.section.buttons.val = {
-  dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+  dashboard.button("f", "  Find file", ":Telescope find_files hidden=true <CR>"),
   -- dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
   dashboard.button("p", "  Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
   -- dashboard.button("r", "  Recent files", ":Telescope oldfiles <CR>"),
