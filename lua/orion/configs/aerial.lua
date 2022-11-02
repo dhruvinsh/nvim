1ab660fbd4f1e479c41a83099e5feb2f7bdda475
Dhruvin Shah <dhruvin3@gmail.com>
2022-11-02 15:45:04 -0400

    plugin(aerial): bump to latest commit

--- lua/orion/configs/aerial.lua
+++ lua/orion/configs/aerial.lua
@@ -4,19 +4,7 @@ if not status_ok then
   return
 end
 
-aerial.setup({
-  attach_mode = "global",
-  backends = { "lsp", "treesitter" },
-  min_width = 28,
-  show_guides = true,
-  filter_kind = false,
-  guides = {
-    mid_item = "├ ",
-    last_item = "└ ",
-    nested_top = "│ ",
-    whitespace = "  ",
-  },
-})
+aerial.setup()
 
 -- if telescope loaded then load the aerial plugin as well.
 pcall(require("telescope").load_extension, "aerial")
