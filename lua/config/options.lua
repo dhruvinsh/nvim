a2e458ec249da58f3eb6f785fc46d6eb0c38bb6b
Dhruvin Shah <dhruvin3@gmail.com>
2023-02-24 20:15:04 -0500

    option: fix node_host_prog

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,5 +3,5 @@
 -- Add any additional options here
 
 if vim.fn.executable("volta") then
-  vim.g.node_host_prog = vim.trim(vim.fn.execute("volta which neovim-node-host"))
+  vim.g.node_host_prog = vim.fn.trim(vim.fn.system("volta which neovim-node-host", true))
 end
