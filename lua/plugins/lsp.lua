bd0f739573c82082800b8bc636254e5c75427115
Dhruvin Shah <dhruvin3@gmail.com>
2023-03-01 00:10:29 -0500

    null-ls: adding some better python support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,4 +18,27 @@ return {
       },
     },
   },
+
+  -- null-ls update
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources, {
+        -- python
+        nls.builtins.formatting.black,
+        nls.builtins.formatting.isort,
+        nls.builtins.diagnostics.mypy,
+        nls.builtins.diagnostics.ruff,
+
+        -- all other js based langs
+        nls.builtins.formatting.prettier,
+      })
+
+      -- remove flake8 from opts.sources
+      opts.sources = vim.tbl_filter(function(source)
+        return source.name ~= "flake8"
+      end, opts.sources)
+    end,
+  },
 }
