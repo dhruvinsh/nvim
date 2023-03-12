b8b2c2fd920fedc1cc8296727925af5ed924f99d
Dhruvin Shah <dhruvin3@gmail.com>
2023-03-12 19:33:25 -0400

    lsp: fixing shfmt

--- lua/plugins/formatting/shell.lua
+++ lua/plugins/formatting/shell.lua
@@ -12,7 +12,7 @@ return {
     "jose-elias-alvarez/null-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
-      table.insert(opts.sources, nls.builtins.diagnostics.shfmt)
+      table.insert(opts.sources, nls.builtins.formatting.shfmt)
     end,
   },
 }
