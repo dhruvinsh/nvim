c01efa58eaaf9fef040d5111c6cb7fded1719f71
Dhruvin Shah <dhruvin3@gmail.com>
2023-03-26 18:08:44 -0400

    lsp: removign null-ls support for clang_format

--- lua/plugins/c.lua
+++ lua/plugins/c.lua
@@ -9,12 +9,4 @@ return {
       })
     end,
   },
-
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      table.insert(opts.sources, nls.builtins.formatting.clang_format)
-    end,
-  },
 }
