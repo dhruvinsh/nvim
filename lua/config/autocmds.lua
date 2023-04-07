67caee2912250c1b5b97a8a9ad0e554eb2f61eb7
Dhruvin Shah <dhruvin3@gmail.com>
2023-04-06 21:04:28 -0400

    utility: closing unused buffer
    Reference:
    https://www.reddit.com/r/neovim/comments/12c4ad8/closing_unused_buffers/

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,28 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Closing unused buffer:
+-- https://www.reddit.com/r/neovim/comments/12c4ad8/closing_unused_buffers/
+local id = vim.api.nvim_create_augroup("startup", {
+  clear = false,
+})
+
+local persistbuffer = function(bufnr)
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+  vim.fn.setbufvar(bufnr, "bufpersist", 1)
+end
+
+vim.api.nvim_create_autocmd({ "BufRead" }, {
+  group = id,
+  pattern = { "*" },
+  callback = function()
+    vim.api.nvim_create_autocmd({ "InsertEnter", "BufModifiedSet" }, {
+      buffer = 0,
+      once = true,
+      callback = function()
+        persistbuffer()
+      end,
+    })
+  end,
+})
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<Leader>bc", function()
+  local curbufnr = vim.api.nvim_get_current_buf()
+  local buflist = vim.api.nvim_list_bufs()
+  for _, bufnr in ipairs(buflist) do
+    if vim.bo[bufnr].buflisted and bufnr ~= curbufnr and (vim.fn.getbufvar(bufnr, "bufpersist") ~= 1) then
+      vim.cmd("bd " .. tostring(bufnr))
+    end
+  end
+end, { silent = true, desc = "Close unused buffers" })
