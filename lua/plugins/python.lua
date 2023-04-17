19cad2e363291ea9494267206e7e4007d8234936
Dhruvin Shah <dhruvin3@gmail.com>
2023-04-16 20:51:15 -0400

    lsp: migrating mason config to nvim-lspconfig

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -1,18 +1,13 @@
 return {
   {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      ---@diagnostic disable: missing-parameter
-      opts.ensure_installed = vim.list_extend(opts.ensure_installed, {
-        -- as a part of standard dotfiles installation these packages are installed
-        -- "mypy",
-        -- "ruff",
-        -- "black",
-        -- "isort",
-        "pyright",
-        "ruff-lsp",
-      })
-    end,
+    "neovim/nvim-lspconfig",
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        pyright = {},
+        ruff_lsp = {},
+      },
+    },
   },
 
   {
@@ -24,11 +19,6 @@ return {
 
       table.insert(opts.sources, nls.builtins.formatting.black)
       table.insert(opts.sources, nls.builtins.formatting.isort)
-
-      -- remove flake8 from opts.sources
-      opts.sources = vim.tbl_filter(function(source)
-        return source.name ~= "flake8"
-      end, opts.sources)
     end,
   },
 }
