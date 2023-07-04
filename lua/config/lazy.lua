425f88fd3efab6974149b892eb6c8d35e6a9abc6
Dhruvin Shah <dhruvin3@gmail.com>
2023-07-03 21:47:55 -0400

    config: rust and c removed in favor of built in support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,8 +13,10 @@ local specifications = {
   -- import any extras modules here
   { import = "lazyvim.plugins.extras.dap.core" },
   { import = "lazyvim.plugins.extras.formatting.prettier" },
+  { import = "lazyvim.plugins.extras.lang.clangd" },
   -- { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.json" },
+  -- { import = "lazyvim.plugins.extras.lang.rust" },
   { import = "lazyvim.plugins.extras.lang.tailwind" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.linting.eslint" },
