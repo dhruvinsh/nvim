80bfdcf0cf0193cb7c0a2ec4ac6bdf7216f85cf7
Dhruvin Shah <dhruvin3@gmail.com>
2023-07-09 13:36:14 -0400

    config: updating python lang

--- lua/plugins/lang/python.lua
+++ lua/plugins/lang/python.lua
@@ -1,5 +1,14 @@
 return {
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "ninja", "python", "rst", "toml" })
+      end
+    end,
+  },
+
+  {
     "neovim/nvim-lspconfig",
     opts = {
       ---@type lspconfig.options
@@ -7,6 +16,16 @@ return {
         pyright = {},
         ruff_lsp = {},
       },
+    },
+    setup = {
+      ruff_lsp = function()
+        require("lazyvim.util").on_attach(function(client, _)
+          if client.name == "ruff_lsp" then
+            -- Disable hover in favor of Pyright
+            client.server_capabilities.hoverProvider = false
+          end
+        end)
+      end,
     },
   },
 
