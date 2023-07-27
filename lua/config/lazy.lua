3b995680aa8dbecf0a58c79c5c595930dff70b1b
Dhruvin Shah <dhruvin3@gmail.com>
2023-07-26 21:14:10 -0400

    some more cleanup

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,18 +13,11 @@ local specifications = {
   -- import any extras modules here
   { import = "lazyvim.plugins.extras.dap.core" },
   { import = "lazyvim.plugins.extras.formatting.prettier" },
-  -- { import = "lazyvim.plugins.extras.lang.clangd" },
-  -- { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.json" },
-  -- { import = "lazyvim.plugins.extras.lang.rust" },
-  -- { import = "lazyvim.plugins.extras.lang.tailwind" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.linting.eslint" },
-  -- { import = "lazyvim.plugins.extras.ui.edgy" },
   { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-  -- { import = "lazyvim.plugins.extras.util.project" },
   { import = "lazyvim.plugins.extras.vscode" },
-  -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
   -- import/override with your plugins
   { import = "plugins" },
   { import = "plugins.lang" },
