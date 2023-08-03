7564d62eea2d0ed5e1036b8a81048fb1f8b4af5b
Dhruvin Shah <dhruvin3@gmail.com>
2023-08-03 17:00:35 -0400

    plugin(s): adding plugin replacement for surround and pairs

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -10,4 +10,22 @@ return {
     "echasnovski/mini.pairs",
     enabled = false,
   },
+
+  -- ([< surround >]) it.
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = { "BufReadPost", "BufNewFile", "InsertEnter" },
+    dependencies = {
+      { "tpope/vim-repeat", lazy = true },
+    },
+    config = true,
+  },
+
+  -- automatic pair insersion
+  {
+    "windwp/nvim-autopairs",
+    event = "VeryLazy",
+    config = true,
+  },
 }
