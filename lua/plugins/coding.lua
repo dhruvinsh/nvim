d901136074dac8241bb554874c6dcd3b1fa24666
Dhruvin Shah <dhruvin3@gmail.com>
2023-08-03 17:01:18 -0400

    plugin: adding toggleterm

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -38,4 +38,24 @@ return {
       { "<leader>gg", "<cmd>Git<cr>", mode = "n", desc = "Git Status" },
     },
   },
+
+  -- I prefere toggleterm over float term of lazyvim
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    cmd = "ToggleTerm",
+    keys = {
+      {
+        "<leader>ft",
+        "<cmd>ToggleTerm dir=" .. require("lazyvim.util").get_root() .. "<cr>",
+        desc = "Terminal (root dir)",
+      },
+      {
+        "<leader>fT",
+        "<cmd>ToggleTerm <cr>",
+        desc = "Terminal (cwd)",
+      },
+    },
+    config = true,
+  },
 }
