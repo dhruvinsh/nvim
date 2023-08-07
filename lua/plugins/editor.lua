369b016c9c751683c60304c41c372d1a8aa88825
Dhruvin Shah <dhruvin3@gmail.com>
2023-08-07 13:45:16 -0400

    editor: moving around telescope config

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -39,4 +39,34 @@ return {
       require("better_escape").setup(opts)
     end,
   },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        vimgrep_arguments = {
+          "rg",
+          "--color=never",
+          "--column",
+          "--hidden",
+          "--line-number",
+          "--no-heading",
+          "--smart-case",
+          "--with-filename",
+        },
+      },
+    },
+  },
+
+  -- add telescope-fzf-native
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
 }
