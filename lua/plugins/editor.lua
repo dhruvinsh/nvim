e2fa47580e199e1b7fb175eabb85f66104729ca8
Dhruvin Shah <dhruvin3@gmail.com>
2023-08-08 22:13:41 -0400

    plugin: telescope live_grep doesn't search for `.git`

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -44,6 +44,7 @@ return {
     "nvim-telescope/telescope.nvim",
     opts = {
       defaults = {
+        file_ignore_patterns = { ".git" },
         vimgrep_arguments = {
           "rg",
           "--color=never",
