1e80fa78fe109f4f35799a4d45eaea06fb8022e2
Dhruvin Shah <dhruvin3@gmail.com>
2023-08-08 22:13:13 -0400

    ui: remove tidal sign on the left

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,5 @@
 if vim.fn.executable("volta") then
   vim.g.node_host_prog = vim.fn.trim(vim.fn.system("volta which neovim-node-host", true))
 end
+
+vim.opt.fillchars = { eob = " " } -- remove tidal sign on left side for non-text buffer
