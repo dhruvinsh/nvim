ca6bd366614b275847fb6cdde0f89893397a6a25
Dhruvin Shah <dhruvin3@gmail.com>
2023-08-09 11:06:38 -0400

    editor: adding telescope-fzf as dependence

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -42,6 +42,14 @@ return {
 
   {
     "nvim-telescope/telescope.nvim",
+    dependencies = {
+      -- add telescope-fzf-native
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
     opts = {
       defaults = {
         file_ignore_patterns = { ".git" },
@@ -56,18 +64,6 @@ return {
           "--with-filename",
         },
       },
-    },
-  },
-
-  -- add telescope-fzf-native
-  {
-    "telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
     },
   },
 
