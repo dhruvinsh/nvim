bb634f864fb54e1584a37c9a8cbd84f545c125ba
Dhruvin Shah <dhruvin3@gmail.com>
2023-08-23 18:47:21 -0400

    autocmds: better augroup functions

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,19 +2,19 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+local augroup = function(name)
+  return vim.api.nvim_create_augroup("orion_" .. name, { clear = true })
+end
+
 -- Closing unused buffer:
 -- https://www.reddit.com/r/neovim/comments/12c4ad8/closing_unused_buffers/
-local id = vim.api.nvim_create_augroup("startup", {
-  clear = false,
-})
-
 local persistbuffer = function(bufnr)
   bufnr = bufnr or vim.api.nvim_get_current_buf()
   vim.fn.setbufvar(bufnr, "bufpersist", 1)
 end
 
 vim.api.nvim_create_autocmd({ "BufRead" }, {
-  group = id,
+  group = augroup("unused_buffer"),
   pattern = { "*" },
   callback = function()
     vim.api.nvim_create_autocmd({ "InsertEnter", "BufModifiedSet" }, {
