b91b782f5f18c496fb3da31c54838024e0cc80ab
Dhruvin Shah <dhruvin3@gmail.com>
2023-08-28 16:08:50 -0400

    lang: adding treesitter kdl support

---
+++ lua/plugins/lang/kdl.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "kdl" })
+      end
+    end,
+  },
+}
