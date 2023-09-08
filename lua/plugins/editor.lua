c9ee4457155d9a8ebeb0a1ef081d31e443a34533
Dhruvin Shah <dhruvin3@gmail.com>
2023-09-08 17:18:19 -0400

    Revert "editor: colemak escape added"
    `ne` is highly used word and I end up escaping the edit mode
    
    This reverts commit 4d336f147607dd66856a90c95c15e7154bdcf3ee.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -54,7 +54,7 @@ return {
     "max397574/better-escape.nvim",
     event = "BufReadPost",
     opts = {
-      mapping = { "jk", "ne" }, -- a table with mappings to use
+      mapping = { "jk" }, -- a table with mappings to use
       timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
       clear_empty_lines = false, -- clear line after escaping if there is only whitespace
     },
