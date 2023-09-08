8ead33ae0bba883b2896f6d72fc30c69a04b1116
Dhruvin Shah <dhruvin3@gmail.com>
2023-09-08 17:04:40 -0400

    lang: devicetree added

---
+++ lua/plugins/lang/devicetree.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "devicetree" })
+      end
+    end,
+  },
+}
