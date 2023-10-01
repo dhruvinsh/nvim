2a380a76889c9c113c069d1591cfe08730eba05a
Dhruvin Shah <dhruvin3@gmail.com>
2023-09-30 20:16:42 -0400

    utils: function to detected big files

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -1,5 +1,16 @@
 local M = {}
 
+---@param bufnr number
+M.is_big_buffer = function(bufnr)
+  local max_filesize = 100 * 1024 -- 100 KB
+  local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
+  if ok and stats and stats.size > max_filesize then
+    return true
+  else
+    return false
+  end
+end
+
 M.persistbuffer = function()
   local bufnr = vim.api.nvim_get_current_buf()
   vim.fn.setbufvar(bufnr, "bufpersist", 1)
