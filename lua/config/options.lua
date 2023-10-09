7f2518e7be8f4c5fff0ab15721aec365d5bb79a3
Dhruvin Shah <dhruvin3@gmail.com>
2023-10-09 15:59:02 -0400

    removing some old config

--- lua/config/options.lua
+++
@@ -1,9 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
-if vim.fn.executable("volta") then
-  vim.g.node_host_prog = vim.fn.trim(vim.fn.system("volta which neovim-node-host", true))
-end
-
-vim.opt.fillchars = { eob = " " } -- remove tidal sign on left side for non-text buffer
