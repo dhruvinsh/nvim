14b8d3968f2899b2008083bb1d83e74d07d87465
Dhruvin Shah <dhruvin3@gmail.com>
2023-10-10 12:24:20 -0400

    core: some better keymaps added

--- lua/orion/config/keymaps.lua
+++ lua/orion/config/keymaps.lua
@@ -2,6 +2,18 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>l", ":Lazy <cr>", { desc = "Lazy" })
 
+-- movement
+vim.keymap.set("i", "<C-a>", "<Home>", { desc = "Beginning of line" })
+vim.keymap.set("i", "<C-e>", "<End>", { desc = "End of line" })
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<leader>wd", "<C-w>q", { desc = "Delete" })
+
+-- editor
+vim.keymap.set({ "n", "i" }, "<C-s>", "<cmd>w<cr>")
+
 vim.keymap.set("n", "<leader>bc", function()
   local curbufnr = vim.api.nvim_get_current_buf()
   local buflist = vim.api.nvim_list_bufs()
@@ -11,4 +23,3 @@ vim.keymap.set("n", "<leader>bc", function()
     end
   end
 end, { silent = true, desc = "Close unused buffers" })
-
