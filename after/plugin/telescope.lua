06674e687ad225ba2009b20c0871b5d7526630b6
Dhruvin Shah <dhruvin3@gmail.com>
2023-10-10 10:01:35 -0400

    config(telescope): adding hidden search, better mapping

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,22 +1,37 @@
+local telescope = require("telescope")
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>/", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader><space>", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
-vim.keymap.set("n", "<leader>sh", builtin.help_tags, {})
-vim.keymap.set("n", "<leader>sw", builtin.grep_string, {})
+telescope.setup({
+  defaults = {
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--column",
+      "--hidden",
+      "--line-number",
+      "--no-heading",
+      "--smart-case",
+      "--with-filename",
+    },
+  },
+})
 
-require('telescope').setup {
+vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Live Grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+vim.keymap.set("n", "<leader><space>", builtin.find_files, { desc = "Find Files" })
+vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Git Files" })
+vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Help" })
+vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "Word" })
+
+require("telescope").setup({
   extensions = {
     fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
-    }
-  }
-}
-require('telescope').load_extension('fzf')
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = "smart_case",
+    },
+  },
+})
+require("telescope").load_extension("fzf")
