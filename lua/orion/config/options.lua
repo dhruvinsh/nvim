98530c801d4ff0e82e2b1efc2ef86249a83896a5
Dhruvin Shah <dhruvin3@gmail.com>
2023-10-10 23:49:12 -0400

    core: some more options added

--- lua/orion/config/options.lua
+++ lua/orion/config/options.lua
@@ -14,9 +14,11 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.smartcase = true
 vim.opt.spelllang = { "en" }
+vim.opt.swapfile = false
 vim.opt.timeoutlen = 200
 vim.opt.undofile = true
 vim.opt.undolevels = 10000
+vim.opt.updatetime = 250
 -- ui
 vim.opt.breakindent = true
 vim.opt.cmdheight = 1
@@ -28,16 +30,9 @@ vim.opt.pumheight = 10
 vim.opt.numberwidth = 2
 vim.opt.scrolloff = 4
 vim.opt.showmode = false
+vim.opt.smoothscroll = true
 vim.opt.splitbelow = true
 vim.opt.splitkeep = "screen"
 vim.opt.splitright = true
 vim.opt.termguicolors = true
-vim.opt.fillchars = {
-  foldopen = "",
-  foldclose = "",
-  -- fold = "⸱",
-  fold = " ",
-  foldsep = " ",
-  diff = "╱",
-  eob = " ",
-}
+vim.opt.fillchars = { eob = " " }
