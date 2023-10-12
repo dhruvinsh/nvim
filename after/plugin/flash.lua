b653f2543e31adc9e1474f5d9760e979b8de1e36
Dhruvin Shah <dhruvin3@gmail.com>
2023-10-11 22:09:48 -0400

    keymap: better support for flash and treesitter

--- after/plugin/flash.lua
+++ after/plugin/flash.lua
@@ -2,7 +2,7 @@ vim.keymap.set({ "n", "x", "o" }, "s", function()
   require("flash").jump()
 end, { desc = "Flash" })
 
-vim.keymap.set({ "n", "x", "o" }, "S", function()
+vim.keymap.set({ "n", "x", "o" }, "<C-Space>", function()
   require("flash").treesitter()
 end, { desc = "Flash Treesitter" })
 
@@ -14,6 +14,7 @@ vim.keymap.set({ "o", "x" }, "R", function()
   require("flash").treesitter_search()
 end, { desc = "Treesitter Search" })
 
+-- in commnad mode toggle the Flash, helpful in case of normal search required.
 vim.keymap.set({ "c" }, "<c-s>", function()
   require("flash").toggle()
 end, { desc = "Toggle Flash Search" })
