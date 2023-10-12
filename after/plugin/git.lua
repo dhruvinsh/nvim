aac08fd77012e28836941ab306064fd9f35b817e
Dhruvin Shah <dhruvin3@gmail.com>
2023-10-12 17:17:20 -0400

    config: git keymap fixed

--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -1,9 +1,20 @@
-local gs = require("gitsigns")
-
 -- fugitive
 vim.keymap.set("n", "<leader>gg", vim.cmd.Git, { desc = "Git Status" })
 
 -- gitsigns
-vim.keymap.set("n", "gP", gs.preview_hunk, { buffer = true, desc = "Preview git hunk" })
-vim.keymap.set({ "n", "v" }, "]h", gs.next_hunk, { buffer = true, desc = "Jump to next hunk" })
-vim.keymap.set({ "n", "v" }, "[h", gs.prev_hunk, { buffer = true, desc = "Jump to previous hunk" })
+local gs = require("gitsigns")
+gs.setup({
+  on_attach = function(bufnr)
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    map("n", "gp", gs.preview_hunk, { desc = "Preview git hunk" })
+    map("n", "gP", gs.preview_hunk_inline, { desc = "Preview git inline" })
+    map({ "n", "v" }, "]h", gs.next_hunk, { desc = "Jump to next hunk" })
+    map({ "n", "v" }, "[h", gs.prev_hunk, { desc = "Jump to previous hunk" })
+    map({ "n", "v" }, "<leader>gb", ":Gitsigns blame_line full=true<cr>", { desc = "Blame" })
+  end,
+})
