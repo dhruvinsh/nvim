c1da064e3f6c8113c702e1213f6c8c149e86cba8
Dhruvin Shah <dhruvin3@gmail.com>
2023-10-11 22:11:00 -0400

    core: some helpful keymaps added

--- lua/orion/config/keymaps.lua
+++ lua/orion/config/keymaps.lua
@@ -1,15 +1,18 @@
 vim.g.mapleader = " "
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 vim.keymap.set("n", "<leader>l", ":Lazy <cr>", { desc = "Lazy" })
 
 -- movement
-vim.keymap.set("i", "<C-a>", "<Home>", { desc = "Beginning of line" })
-vim.keymap.set("i", "<C-e>", "<End>", { desc = "End of line" })
+vim.keymap.set("i", "<C-a>", "<Home>")
+vim.keymap.set("i", "<C-e>", "<End>")
 vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<leader>wd", "<C-w>q", { desc = "Delete" })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
 -- editor
 vim.keymap.set({ "n", "i" }, "<C-s>", "<cmd>w<cr>")
