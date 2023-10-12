23bbc88ab56c7de72518a361ea9e39ac8777195e
Dhruvin Shah <dhruvin3@gmail.com>
2023-10-11 22:10:16 -0400

    keymap: better telescope mappings

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -16,14 +16,25 @@ telescope.setup({
   },
 })
 
+local buffer_search = function()
+  builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+    winblend = 10,
+    previewer = false,
+  }))
+end
+
 vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Live Grep" })
+vim.keymap.set("n", "<leader><space>", builtin.find_files, { desc = "Find Files" })
+vim.keymap.set("n", "<leader>bs", buffer_search, { desc = "Search" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
-vim.keymap.set("n", "<leader><space>", builtin.find_files, { desc = "Find Files" })
 vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Git Files" })
+vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent Files" })
 vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Help" })
 vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "Word" })
+vim.keymap.set("n", "<leader>tC", "<cmd>Telescope colorscheme enable_preview=true<cr>", { desc = "Colorscheme" })
 
+-- fzf setup
 require("telescope").setup({
   extensions = {
     fzf = {
