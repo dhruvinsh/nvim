abade7ac5b9ceec679d45c178cacd274744e23c1
Dhruvin Shah <dhruvin3@gmail.com>
2023-10-12 10:43:02 -0400

    config(git): better keymap

--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -3,23 +3,7 @@ local gs = require("gitsigns")
 -- fugitive
 vim.keymap.set("n", "<leader>gg", vim.cmd.Git, { desc = "Git Status" })
 
-vim.keymap.set("n", "<leader>gsp", gs.preview_hunk, { buffer = bufnr, desc = "Preview git hunk" })
-
-vim.keymap.set({ "n", "v" }, "]c", function()
-  if vim.wo.diff then
-    return "]c"
-  end
-  vim.schedule(function()
-    gs.next_hunk()
-  end)
-  return "<Ignore>"
-end, { expr = true, buffer = bufnr, desc = "Jump to next hunk" })
-vim.keymap.set({ "n", "v" }, "[c", function()
-  if vim.wo.diff then
-    return "[c"
-  end
-  vim.schedule(function()
-    gs.prev_hunk()
-  end)
-  return "<Ignore>"
-end, { expr = true, buffer = bufnr, desc = "Jump to previous hunk" })
+-- gitsigns
+vim.keymap.set("n", "gP", gs.preview_hunk, { buffer = true, desc = "Preview git hunk" })
+vim.keymap.set({ "n", "v" }, "]h", gs.next_hunk, { buffer = true, desc = "Jump to next hunk" })
+vim.keymap.set({ "n", "v" }, "[h", gs.prev_hunk, { buffer = true, desc = "Jump to previous hunk" })
