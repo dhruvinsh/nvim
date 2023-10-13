574657d5a4fbdf1003212a90de43b242145e0999
Dhruvin Shah <dhruvin3@gmail.com>
2023-10-13 09:16:51 -0400

    utils: rearranging code a little better

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -1,26 +1,30 @@
 local M = {}
 
-M.root_patterns = { ".git", "Makefile", "pyproject.toml", ".projectile", ".neoconf.json", ".editorconfig" }
 M.max_filesize = 512 * 1024 -- big file size threshold 0.5 MB
+M.os_name = vim.loop.os_uname().sysname
+M.root_patterns = { ".git", "Makefile", "pyproject.toml", ".projectile", ".neoconf.json", ".editorconfig" }
 
+M.is_mac = M.os_name == "Darwin"
+
+---@param name string
+---@return integer
 M.augroup = function(name)
   return vim.api.nvim_create_augroup("orion_" .. name, { clear = true })
 end
 
-M.os_name = vim.loop.os_uname().sysname
-
-M.is_mac = M.os_name == "Darwin"
-
 ---@param bufnr number
+---@return boolean
 M.is_big_buffer = function(bufnr)
+  -- here fs_stat needs file path to identify file size.
   local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
   if ok and stats and stats.size > M.max_filesize then
     return true
-  else
-    return false
   end
+  return false
 end
 
+---@param tbl table
+---@param ... string | integer
 ---@return nil
 M.tbl_append = function(tbl, ...)
   for _, value in ipairs({ ... }) do
