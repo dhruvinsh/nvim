a1f8cf565dc5120c20aaaaafc13f4f4e2edae89b
Dhruvin Shah <dhruvin3@gmail.com>
2023-10-12 21:01:04 -0400

    utils: extracting max file size

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 M.root_patterns = { ".git", "Makefile", "pyproject.toml", ".projectile", ".neoconf.json", ".editorconfig" }
+M.max_filesize = 512 * 1024 -- big file size threshold 0.5 MB
 
 M.augroup = function(name)
   return vim.api.nvim_create_augroup("orion_" .. name, { clear = true })
@@ -12,9 +13,8 @@ M.is_mac = M.os_name == "Darwin"
 
 ---@param bufnr number
 M.is_big_buffer = function(bufnr)
-  local max_filesize = 100 * 1024 -- 100 KB
   local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
-  if ok and stats and stats.size > max_filesize then
+  if ok and stats and stats.size > M.max_filesize then
     return true
   else
     return false
