8429994a2c48536881e5d1d684b619ee909bc29c
Dhruvin Shah <dhruvin3@gmail.com>
2023-10-14 18:38:33 -0400

    config: persistence keymap desc added

--- after/plugin/persistence.lua
+++ after/plugin/persistence.lua
@@ -3,10 +3,15 @@ require("persistence").setup({
 })
 
 -- restore the session for the current directory
-vim.api.nvim_set_keymap("n", "<leader>qs", [[<cmd>lua require("persistence").load()<cr>]], {})
+vim.api.nvim_set_keymap("n", "<leader>qs", [[<cmd>lua require("persistence").load()<cr>]], { desc = "Current Session" })
 
 -- restore the last session
-vim.api.nvim_set_keymap("n", "<leader>ql", [[<cmd>lua require("persistence").load({ last = true })<cr>]], {})
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>ql",
+  [[<cmd>lua require("persistence").load({ last = true })<cr>]],
+  { desc = "Last Session" }
+)
 
 -- stop Persistence => session won't be saved on exit
-vim.api.nvim_set_keymap("n", "<leader>ts", [[<cmd>lua require("persistence").stop()<cr>]], {})
+vim.api.nvim_set_keymap("n", "<leader>ts", [[<cmd>lua require("persistence").stop()<cr>]], { desc = "Session Stop" })
