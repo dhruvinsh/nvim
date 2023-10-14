6e6e49969a9b4f5220bc012d54fab02eb3dfe77f
Dhruvin Shah <dhruvin3@gmail.com>
2023-10-14 14:46:30 -0400

    config(bufferline): keymaps added

---
+++ after/plugin/bufferline.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
+vim.keymap.set("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
+vim.keymap.set("n", "[b", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
+vim.keymap.set("n", "]b", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
