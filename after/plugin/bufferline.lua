e6b3bb7a2877b9c1d098ca05aab42e20a24b3000
Dhruvin Shah <dhruvin3@gmail.com>
2023-10-14 21:33:25 -0400

    config: better bufferline

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,4 +1,36 @@
+local ui = require("utils.ui")
+
+---@diagnostic disable:missing-fields
+require("bufferline").setup({
+  options = {
+    close_command = function(n)
+      require("mini.bufremove").delete(n, false)
+    end,
+    right_mouse_command = function(n)
+      require("mini.bufremove").delete(n, false)
+    end,
+    diagnostics = "nvim_lsp",
+    always_show_bufferline = false,
+    diagnostics_indicator = function(_, _, diag)
+      local icons = ui.diagnostics
+      local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+        .. (diag.warning and icons.Warn .. diag.warning or "")
+      return vim.trim(ret)
+    end,
+    offsets = {
+      {
+        filetype = "neo-tree",
+        text = "Neo-tree",
+        highlight = "Directory",
+        text_align = "left",
+      },
+    },
+  },
+})
+
 vim.keymap.set("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
 vim.keymap.set("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", { desc = "Delete non-pinned buffers" })
+vim.keymap.set("n", "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", { desc = "Toggle pin" })
 vim.keymap.set("n", "[b", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
 vim.keymap.set("n", "]b", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
