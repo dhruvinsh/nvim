aa2fa1ce40e17e36bbcc57798c1323e05e5b2b28
Dhruvin Shah <dhruvin3@gmail.com>
2023-10-15 22:35:58 -0400

    config(dashboard): some new keymap added

--- after/plugin/dashboard.lua
+++ after/plugin/dashboard.lua
@@ -1,5 +1,34 @@
 local db = require("dashboard")
 
+local config_path = vim.fn.stdpath("config")
+
+-- db.setup({
+--   theme = "doom",
+--   change_to_vcs_root = true,
+--   config = {
+--     header = vim.split(string.rep("\n", 8) .. "" .. "\n\n", "\n"),
+--     -- week_header = {
+--     --   enable = true,
+--     -- },
+--     -- stylua: ignore
+--     center = {
+--       { action = "Telescope find_files",                     desc = " Find file", icon = " ", key = "f" },
+--       { action = "ene | startinsert",                        desc = " New file",  icon = " ", key = "n" },
+--       { action = 'lua require("persistence").load()',        desc = " Session",   icon = " ", key = "s" },
+--       { action = "Telescope find_files cwd=" .. config_path, desc = " Config",    icon = " ", key = "c" },
+--       { action = "Lazy update",                              desc = " Update",    icon = "󰊳 ", key = "l" },
+--       { action = "qa",                                       desc = " Quit",      icon = " ", key = "q" },
+--     },
+--     footer = function()
+--       local stats = require("lazy").stats()
+--       local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+--       return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+--     end,
+--   },
+-- })
+
+-- NOTE: old hyper theme
+
 db.setup({
   theme = "hyper",
   change_to_vcs_root = true,
@@ -31,6 +60,14 @@ db.setup({
         group = "@string.escape",
         action = 'lua require("persistence").load()',
         key = "s",
+      },
+      {
+        icon = " ",
+        icon_hl = "@variable",
+        desc = "Config",
+        group = "@class",
+        action = "Telescope find_files dir=" .. config_path,
+        key = "c",
       },
       {
         icon = "󰊳 ",
