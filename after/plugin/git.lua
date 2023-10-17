ccba811a872895ac3b3063ab4c2ccf5ada0d76e6
Dhruvin Shah <dhruvin3@gmail.com>
2023-10-16 21:25:40 -0400

    config(git): some useful keymaps added

--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -33,6 +33,38 @@ gs.setup({
     map("n", "gP", gs.preview_hunk_inline, { desc = "Preview git inline" })
     map({ "n", "v" }, "]h", gs.next_hunk, { desc = "Jump to next hunk" })
     map({ "n", "v" }, "[h", gs.prev_hunk, { desc = "Jump to previous hunk" })
-    map({ "n", "v" }, "<leader>gb", ":Gitsigns blame_line full=true<cr>", { desc = "Blame" })
+
+    map("n", "<leader>gR", "<cmd>Gitsigns reset_buffer<cr>", { desc = "Reset Buffer" })
+    map("n", "<leader>gS", "<cmd>Gitsigns stage_buffer<cr>", { desc = "Stage Buffer" })
+    map({ "n", "v" }, "<leader>gb", "<cmd>Gitsigns blame_line full=true<cr>", { desc = "Blame(full)" })
+    map("n", "<leader>gd", "<cmd>Gitsigns diffthis<cr>", { desc = "Diff" })
+    map("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>", { desc = "Preview Hunk" })
+    map("n", "<leader>gr", "<cmd>Gitsigns reset_hunk<cr>", { desc = "Reset Hunk" })
+    map("n", "<leader>gs", "<cmd>Gitsigns stage_hunk<cr>", { desc = "Stage Hunk" })
+    map("n", "<leader>gu", "<cmd>Gitsigns undo_stage_hunk<cr>", { desc = "Undo Hunk" })
+    map("n", "<leader>tb", "<cmd>Gitsigns toggle_current_line_blame<cr>", { desc = "Toggle Blame" })
+    map("n", "<leader>td", "<cmd>Gitsigns toggle_deleted<cr>", { desc = "Git Delete" })
+    map("v", "<leader>gr", function()
+      gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+    end)
+    map("v", "<leader>gs", function()
+      gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+    end)
+
+    -- Text object
+    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
   end,
 })
+
+-- diffview
+require("diffview").setup({})
+
+-- TODO: inmplement proper api here
+-- local IsDiffViewOpen = false
+-- local function DiffViewToggle()
+--   if IsDiffViewOpen then
+--     vim.cmd.DiffViewOpen
+--   end
+-- end
+
+vim.keymap.set("n", "<leader>gD", "<cmd>DiffviewOpen<CR>", { desc = "Diffview" })
