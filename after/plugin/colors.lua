c10eb7a79b410f79b09abb6e880c7aaa2fd88c54
Dhruvin Shah <dhruvin3@gmail.com>
2023-10-24 23:12:08 -0400

    config: adding catppuccin

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -12,6 +12,28 @@ local color_schemes = {
       style = "warmer",
     })
   end,
+  ["catppuccin"] = function()
+    require("catppuccin").setup({
+      flavour = "mocha",
+      integrations = {
+        cmp = true,
+        dashboard = true,
+        fidget = true,
+        flash = true,
+        gitsigns = true,
+        illuminate = true,
+        lsp_trouble = true,
+        mason = true,
+        mini = { enabled = true, indentscope_color = "" },
+        neotree = true,
+        notify = true,
+        telescope = { enabled = true, style = "nvchad" },
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    })
+  end,
 }
 
 ---A global function that allow to set color scheme
