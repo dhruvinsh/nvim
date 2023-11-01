a2b9aabe1f8f826f12af67cffaca60502bf4a4f7
Dhruvin Shah <dhruvin3@gmail.com>
2023-11-01 08:49:13 -0400

    pluign: nvim-cmp signature help added

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -57,6 +57,7 @@ cmp.setup({
     { name = "nvim_lsp" },
     { name = "luasnip" },
     { name = "path" },
+    { name = "nvim_lsp_signature_help" },
   }, {
     { name = "buffer" },
   }),
--- lua/orion/plugins/coding.lua
+++ lua/orion/plugins/coding.lua
@@ -43,6 +43,7 @@ return {
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
       "hrsh7th/cmp-path",
       "petertriho/cmp-git",
 
