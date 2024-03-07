3341bdc4feab63d9036a6758b99a0cf684442fae
Dhruvin Shah <dhruvin3@gmail.com>
2024-03-07 17:31:38 -0500

    feat(git): add proper git toggle command

--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -1,6 +1,19 @@
 local utils = require("utils")
+
+local toggle_fugitive = function()
+  local winids = vim.api.nvim_list_wins()
+  for _, id in pairs(winids) do
+    local status = pcall(vim.api.nvim_win_get_var, id, "fugitive_status")
+    if status then
+      vim.api.nvim_win_close(id, false)
+      return
+    end
+  end
+  vim.cmd("Git")
+end
+
 -- fugitive
-vim.keymap.set("n", "<leader>gg", vim.cmd.Git, { desc = "Git Status" })
+vim.keymap.set("n", "<leader>gg", toggle_fugitive, { desc = "Git Status" })
 vim.keymap.set("n", "<leader>gD", "<cmd>Gvdiffsplit!<CR>", { desc = "Diff Split" })
 
 vim.api.nvim_create_autocmd("BufWinEnter", {
