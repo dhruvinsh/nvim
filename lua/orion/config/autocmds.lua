5c559e2f78d9aee630bed9b2eb64abbcf0ca5554
Dhruvin Shah <dhruvin3@gmail.com>
2024-03-07 16:37:41 -0500

    feat(autocmds): close man file easily

--- lua/orion/config/autocmds.lua
+++ lua/orion/config/autocmds.lua
@@ -67,7 +67,6 @@ vim.api.nvim_create_autocmd("FileType", {
     "git",
     "help",
     "lspinfo",
-    "man",
     "neotest-output",
     "neotest-output-panel",
     "neotest-summary",
@@ -82,4 +81,11 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.bo[event.buf].buflisted = false
     vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
   end,
+})
+
+-- make it easier to close man-files when opened inline
+vim.api.nvim_create_autocmd("FileType", {
+  group = utils.augroup("man_unlisted"),
+  pattern = { "man" },
+  callback = function(event) vim.bo[event.buf].buflisted = false end,
 })
