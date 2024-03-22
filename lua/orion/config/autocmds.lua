ad2da51ce4f597ec372b6e8a759a4df8a03173ba
Dhruvin Shah <dhruvin3@gmail.com>
2024-03-22 13:25:43 -0400

    feat(autocmds): close fugitive blame as well

--- lua/orion/config/autocmds.lua
+++ lua/orion/config/autocmds.lua
@@ -68,6 +68,7 @@ vim.api.nvim_create_autocmd("FileType", {
     "spectre_panel",
     "startuptime",
     "tsplayground",
+    "fugitiveblame",
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
