c99551a39100de0bd6e8c9ebc86ac6f7e4dac0b1
Dhruvin Shah <dhruvin3@gmail.com>
2024-03-30 15:56:59 -0400

    feat(colors): rose-pine better lsp-semantic

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -7,8 +7,59 @@ local color_schemes = {
       highlight_groups = {
         Comment = { italic = true },
 
-        -- fix for python basedpyright
-        ["@lsp.type.variable"] = {},
+        -- from catppuccin
+        ["@lsp.type.unresolvedReference"] = { link = "@error" },
+        ["@lsp.typemod.type.defaultLibrary"] = { link = "@type.builtin" },
+
+        -- from bamboo
+        ["@lsp.typemod.typeAlias.defaultLibrary"] = { link = "@type.builtin" },
+
+        -- not using from tokyonight
+        -- ["@lsp.type.interface"] = { fg = util.lighten(c.blue1, 0.7) },
+        -- ["@lsp.typemod.type.defaultLibrary"] = { fg = util.darken(c.blue1, 0.8) },
+        -- ["@lsp.typemod.typeAlias.defaultLibrary"] = { fg = util.darken(c.blue1, 0.8) },
+
+        ["@lsp.type.boolean"] = { link = "@boolean" },
+        ["@lsp.type.builtinType"] = { link = "@type.builtin" },
+        ["@lsp.type.comment"] = { link = "@comment" },
+        ["@lsp.type.decorator"] = { link = "@attribute" },
+        ["@lsp.type.deriveHelper"] = { link = "@attribute" },
+        ["@lsp.type.enum"] = { link = "@type" },
+        ["@lsp.type.enumMember"] = { link = "@constant" },
+        ["@lsp.type.escapeSequence"] = { link = "@string.escape" },
+        ["@lsp.type.formatSpecifier"] = { link = "@markup.list" },
+        ["@lsp.type.generic"] = { link = "@variable" },
+        ["@lsp.type.keyword"] = { link = "@keyword" },
+        ["@lsp.type.lifetime"] = { link = "@keyword.storage" },
+        ["@lsp.type.namespace"] = { link = "@module" },
+        ["@lsp.type.number"] = { link = "@number" },
+        ["@lsp.type.operator"] = { link = "@operator" },
+        ["@lsp.type.parameter"] = { link = "@variable.parameter" },
+        ["@lsp.type.property"] = { link = "@property" },
+        ["@lsp.type.selfKeyword"] = { link = "@variable.builtin" },
+        ["@lsp.type.selfTypeKeyword"] = { link = "@variable.builtin" },
+        ["@lsp.type.string"] = { link = "@string" },
+        ["@lsp.type.typeAlias"] = { link = "@type.definition" },
+        ["@lsp.type.variable"] = {}, -- use treesitter styles for regular variables
+        ["@lsp.typemod.class.defaultLibrary"] = { link = "@type.builtin" },
+        ["@lsp.typemod.enum.defaultLibrary"] = { link = "@type.builtin" },
+        ["@lsp.typemod.enumMember.defaultLibrary"] = { link = "@constant.builtin" },
+        ["@lsp.typemod.function.defaultLibrary"] = { link = "@function.builtin" },
+        ["@lsp.typemod.keyword.async"] = { link = "@keyword.coroutine" },
+        ["@lsp.typemod.keyword.injected"] = { link = "@keyword" },
+        ["@lsp.typemod.macro.defaultLibrary"] = { link = "@function.builtin" },
+        ["@lsp.typemod.method.defaultLibrary"] = { link = "@function.builtin" },
+        ["@lsp.typemod.operator.injected"] = { link = "@operator" },
+        ["@lsp.typemod.string.injected"] = { link = "@string" },
+        ["@lsp.typemod.struct.defaultLibrary"] = { link = "@type.builtin" },
+        ["@lsp.typemod.variable.callable"] = { link = "@function" },
+        ["@lsp.typemod.variable.defaultLibrary"] = { link = "@variable.builtin" },
+        ["@lsp.typemod.variable.injected"] = { link = "@variable" },
+        ["@lsp.typemod.variable.static"] = { link = "@constant" },
+        -- NOTE: maybe add these with distinct highlights?
+        -- ["@lsp.typemod.variable.globalScope"] (global variables)
+
+        -- Python
         ["@lsp.type.namespace.python"] = { link = "@variable" },
       },
     })
