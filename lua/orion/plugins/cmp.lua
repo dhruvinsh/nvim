cdc20978c272058885afc33bbf962eb3706d0ebe
Dhruvin Shah <dshah@evertz.com>
2024-05-06 10:21:15 -0400

    chore(cmp): disabled enabled option
    - causes more issue than it resolves it. Due to enable behavior I get
      the issue like TelescopePrompt having autocompletion.

--- lua/orion/plugins/cmp.lua
+++ lua/orion/plugins/cmp.lua
@@ -27,14 +27,6 @@ return {
     local compare = require("cmp.config.compare")
 
     cmp.setup({
-      enabled = function()
-        local context = require("cmp.config.context")
-        if vim.api.nvim_get_mode().mode == "c" then
-          return true
-        else
-          return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
-        end
-      end,
       completion = {
         completeopt = "menu,menuone,noinsert",
       },
