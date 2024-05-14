1f38b5ab7e3bc95bc8faa4615f4666742d752a11
Dhruvin Shah <dhruvin3@gmail.com>
2024-05-14 15:55:51 -0400

    feat(colorschems): back to roots - onedark

--- lua/orion/plugins/colorschems.lua
+++ lua/orion/plugins/colorschems.lua
@@ -1,24 +1,12 @@
 return {
-  "RRethy/base16-nvim",
+  "navarasu/onedark.nvim",
   priority = 1000,
   config = function()
-    require("base16-colorscheme").setup({
-      base00 = "#16161D",
-      base01 = "#3e4451",
-      base02 = "#2c313c",
-      base03 = "#565c64",
-      base04 = "#6c7891",
-      base05 = "#abb2bf",
-      base06 = "#9a9bb3",
-      base07 = "#c5c8e6",
-      base08 = "#e06c75",
-      base09 = "#d19a66",
-      base0A = "#e5c07b",
-      base0B = "#98c379",
-      base0C = "#56b6c2",
-      base0D = "#0184bc",
-      base0E = "#c678dd",
-      base0F = "#a06949",
+    require("onedark").setup({
+      colors = {
+        bg0 = "#16161D",
+      },
     })
+    require("onedark").load()
   end,
 }
