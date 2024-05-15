01373a74c0f0381817c82e4ea72d2f80ecc7a031
Dhruvin Shah <dhruvin3@gmail.com>
2024-05-14 22:03:00 -0400

    feat(dashboard): cleaner UI

--- after/plugin/dashboard.lua
+++ after/plugin/dashboard.lua
@@ -2,91 +2,35 @@ local db = require("dashboard")
 local utils = require("utils")
 
 local config_path = vim.fn.stdpath("config")
-
--- db.setup({
---   theme = "doom",
---   change_to_vcs_root = true,
---   config = {
---     header = vim.split(string.rep("\n", 8) .. "" .. "\n\n", "\n"),
---     -- week_header = {
---     --   enable = true,
---     -- },
---     -- stylua: ignore
---     center = {
---       { action = "Telescope find_files",                     desc = " Find file", icon = " ", key = "f" },
---       { action = "ene | startinsert",                        desc = " New file",  icon = " ", key = "n" },
---       { action = 'lua require("persistence").load()',        desc = " Session",   icon = " ", key = "s" },
---       { action = "Telescope find_files cwd=" .. config_path, desc = " Config",    icon = " ", key = "c" },
---       { action = "Lazy update",                              desc = " Update",    icon = "󰊳 ", key = "l" },
---       { action = "qa",                                       desc = " Quit",      icon = " ", key = "q" },
---     },
---     footer = function()
---       local stats = require("lazy").stats()
---       local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
---       return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
---     end,
---   },
--- })
-
--- NOTE: old hyper theme
+local logo = [[
+   ██████╗ ██████╗ ██╗ ██████╗ ███╗   ██╗
+  ██╔═══██╗██╔══██╗██║██╔═══██╗████╗  ██║
+  ██║   ██║██████╔╝██║██║   ██║██╔██╗ ██║
+  ██║   ██║██╔══██╗██║██║   ██║██║╚██╗██║
+  ╚██████╔╝██║  ██║██║╚██████╔╝██║ ╚████║
+  ╚═════╝ ╚═╝  ╚═╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝ 
+]]
 
 db.setup({
-  theme = "hyper",
+  theme = "doom",
   change_to_vcs_root = true,
   config = {
-    week_header = {
-      enable = true,
+    header = vim.split(string.rep("\n", 8) .. logo .. "\n\n", "\n"),
+    -- stylua: ignore
+    center = {
+      { action = "Telescope find_files",                     desc = " Find file", icon = " ", key = "f" },
+      { action = "Telescope projects",                       desc = " Projects",  icon = " ", key = "p" },
+      { action = "ene | startinsert",                        desc = " New file",  icon = " ", key = "n" },
+      { action = "SessionManager load_current_dir_session",  desc = " Session",   icon = "󰳿 ", key = "s" },
+      { action = "Telescope find_files cwd=" .. config_path, desc = " Config",    icon = " ", key = "c" },
+      { action = "Lazy restore",                             desc = " Update",    icon = "󰊳 ", key = "r" },
+      { action = "silent qa",                                desc = " Quit",      icon = " ", key = "q" },
     },
-    shortcut = {
-      {
-        icon = " ",
-        icon_hl = "@variable",
-        desc = "Files",
-        group = "Number",
-        action = "Telescope find_files",
-        key = "f",
-      },
-      {
-        icon = " ",
-        icon_hl = "@variable",
-        desc = "Projects",
-        group = "@method",
-        action = "Telescope projects",
-        key = "p",
-      },
-      {
-        icon = "󰳿 ",
-        icon_hl = "@variable",
-        desc = "Session",
-        group = "@string.escape",
-        action = "SessionManager load_current_dir_session",
-        key = "s",
-      },
-      {
-        icon = " ",
-        icon_hl = "@variable",
-        desc = "Config",
-        group = "@class",
-        action = "Telescope find_files dir=" .. config_path,
-        key = "c",
-      },
-      {
-        icon = "󰊳 ",
-        icon_hl = "@variable",
-        desc = "Restore",
-        group = "@variable.builtin",
-        action = "Lazy restore",
-        key = "r",
-      },
-      {
-        icon = " ",
-        icon_hl = "@variable",
-        desc = "Close",
-        group = "@parameter",
-        action = "silent qa",
-        key = "q",
-      },
-    },
+    footer = function()
+      local stats = require("lazy").stats()
+      local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+      return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+    end,
   },
 })
 
