b52edb00e9be4158e503fc6a15b0b62962dab278
Dhruvin Shah <dhruvin3@gmail.com>
2024-05-14 22:57:23 -0400

    refactor(plugin): treesj

---
+++ lua/orion/plugins/treesj.lua
@@ -0,0 +1,8 @@
+return {
+  "Wansmer/treesj",
+  keys = {
+    { "gS", "<cmd>TSJSplit <cr>", desc = "Split" },
+    { "gJ", "<cmd>TSJJoin <cr>", desc = "Join" },
+  },
+  opts = { use_default_keymaps = false, max_join_length = 150 },
+}
