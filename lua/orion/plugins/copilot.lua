8301f5cee842791adc22255d543d2183ecad4c06
Dhruvin Shah <dshah@evertz.com>
2024-05-16 08:50:48 -0400

    refactor: better copilot config

--- lua/orion/plugins/copilot.lua
+++ lua/orion/plugins/copilot.lua
@@ -4,7 +4,6 @@ return {
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
-    event = "InsertEnter",
     build = ":Copilot auth",
     opts = {
       suggestion = { enabled = false },
@@ -25,9 +24,37 @@ return {
       },
     },
   },
+
   {
-    "zbirenbaum/copilot-cmp",
-    dependencies = "copilot.lua",
-    opts = {},
+    "nvim-cmp",
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        dependencies = "copilot.lua",
+        opts = {},
+        config = function(_, opts)
+          local copilot_cmp = require("copilot_cmp")
+          local utils = require("utils")
+
+          copilot_cmp.setup(opts)
+          -- attach cmp source whenever copilot attaches
+          -- fixes lazy-loading issues with the copilot cmp source
+          vim.api.nvim_create_autocmd("LspAttach", {
+            group = utils.augroup("copilot_attach"),
+            callback = function(ev)
+              local client = vim.lsp.get_client_by_id(ev.data.client_id)
+              if client ~= nil and client.name == "copilot" then copilot_cmp._on_insert_enter({}) end
+            end,
+          })
+        end,
+      },
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, 1, {
+        name = "copilot",
+        group_index = 1,
+      })
+    end,
   },
 }
