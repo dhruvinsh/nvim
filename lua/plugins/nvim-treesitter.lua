d9b112593ca19a74434fa41d2ab6265952faebbc
Dhruvin Shah <dhruvin3@gmail.com>
2024-05-29 15:46:47 -0400

    fix(treesitter): extend default config for ensure_installed

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
+  opts = function(_, opts)
+    vim.list_extend(opts.ensure_installed, {
       "bash",
       "c",
       "devicetree",
@@ -34,6 +34,6 @@ return {
       "vim",
       "vimdoc",
       "yaml",
-    },
-  },
+    })
+  end,
 }
