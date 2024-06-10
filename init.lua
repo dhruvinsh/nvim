77f966c7dcdb92887ab7d07c7429e14f7f697752
Dhruvin Shah <dshah@evertz.com>
2024-06-10 10:25:23 -0400

    refactor(config): minimal vscode version
    * with changes on the vscode-nvim, lua/vscode not allowed.

--- init.lua
+++ init.lua
@@ -1,1 +1,12 @@
-require("vscode")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+require("lazy").setup({
+  "folke/flash.nvim",
+  "kylechui/nvim-surround",
+})
