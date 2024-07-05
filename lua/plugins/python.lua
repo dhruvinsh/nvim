81aa8b5ab492dbfce83989f3e2d12b3de29a6df6
Dhruvin Shah <dhruvin3@gmail.com>
2024-07-05 12:00:43 -0400

    feat(python): update to config
    * ruff_lsp changed to ruff
    * removed semshi

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -13,7 +13,7 @@ return {
     opts = {
       servers = {
         basedpyright = {},
-        ruff_lsp = {
+        ruff = {
           keys = {
             { "<leader>co", LazyVim.lsp.action["source.organizeImports"], desc = "Organize Imports" },
           },
@@ -24,7 +24,7 @@ return {
           LazyVim.lsp.on_attach(function(client, _)
             -- Disable hover in favor of Pyright
             client.server_capabilities.hoverProvider = false
-          end, "ruff_lsp")
+          end, "ruff")
         end,
       },
     },
@@ -58,38 +58,5 @@ return {
         python = { "mypy" },
       },
     },
-  },
-
-  {
-    "wookayin/semshi", -- use a maintained fork
-    ft = "python",
-    build = ":UpdateRemotePlugins",
-    init = function()
-      -- Disabled these features better provided by LSP or other more general plugins
-      vim.g["semshi#error_sign"] = false
-      vim.g["semshi#simplify_markup"] = false
-      vim.g["semshi#mark_selected_nodes"] = false
-      vim.g["semshi#update_delay_factor"] = 0.001
-
-      -- This autocmd must be defined in init to take effect
-      vim.api.nvim_create_autocmd({ "VimEnter", "ColorScheme" }, {
-        group = vim.api.nvim_create_augroup("SemanticHighlight", {}),
-        callback = function()
-          -- Only add style, inherit or link to the LSP's colors
-          vim.cmd([[
-            highlight! link semshiGlobal  @none
-            highlight! link semshiImported @none
-            highlight! link semshiParameter @lsp.type.parameter
-            highlight! link semshiBuiltin @function.builtin
-            highlight! link semshiAttribute @field
-            highlight! link semshiSelf @lsp.type.selfKeyword
-            highlight! link semshiUnresolved @none
-            highlight! link semshiFree @none
-            highlight! link semshiAttribute @none
-            highlight! link semshiParameterUnused @none
-            ]])
-        end,
-      })
-    end,
   },
 }
