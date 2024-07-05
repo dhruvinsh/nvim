e0a6b94dfe0db75d84dd75a1be63135d3e0fdbbe
Dhruvin Shah <dhruvin3@gmail.com>
2024-07-05 19:26:06 -0400

    feat(config): adding back tokyonight

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,8 +30,8 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "onedark", "habamax" } },
   checker = { enabled = false }, -- automatically check for plugin updates
+  install = { colorscheme = { "tokyonight", "habamax" } },
   performance = {
     rtp = {
       -- disable some rtp plugins
