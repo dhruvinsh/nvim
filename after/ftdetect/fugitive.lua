c97f301fdfe5c1c7a092274e6d4e9bd0e03ac401
Dhruvin Shah <dshah@evertz.com>
2024-07-29 16:51:48 -0400

    feat(fugitive): adding back some keymaps

---
+++ after/ftdetect/fugitive.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<leader>gP", "<cmd>Git push<cr>", { desc = "push" })
+vim.keymap.set("n", "<leader>gp", "<cmd>Git pull<cr>", { desc = "pull" })
+vim.keymap.set("n", "q", "<cmd>quit<cr>", { desc = "close", silent = true })
