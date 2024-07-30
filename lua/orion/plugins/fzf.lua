fe35f7a84e501df4cd19335a4a07cd81c43c3f6a
Dhruvin Shah <dhruvin3@gmail.com>
2024-07-30 08:21:23 -0400

    feat(fzf): adding neovim plugin files search

--- lua/orion/plugins/fzf.lua
+++ lua/orion/plugins/fzf.lua
@@ -4,14 +4,15 @@ return {
   dependencies = { "echasnovski/mini.icons" },
   keys = {
     -- stylua: ignore start
-    {"<leader>/",       "<cmd>FzfLua live_grep<cr>",   desc ="grep"},
-    {"<leader><space>", "<cmd>FzfLua files<cr>",       desc ="files"},
-    {"<leader>fb",      "<cmd>FzfLua buffers<cr>",     desc ="buffers"},
-    {"<leader>fc",      "<cmd>FzfLua git_commits<cr>", desc ="commits"},
-    {"<leader>fg",      "<cmd>FzfLua git_files<cr>",   desc ="git files"},
-    {"<leader>ff",      "<cmd>FzfLua files<cr>",       desc ="files"},
-    {"<leader>sh",      "<cmd>FzfLua helptags<cr>",    desc = "help"},
-    {"<leader>sk",      "<cmd>FzfLua keymaps<cr>",     desc ="keymaps"},
+    {"<leader>/",       "<cmd>FzfLua live_grep<cr>",                                                                   desc  ="grep"},
+    {"<leader><space>", "<cmd>FzfLua files<cr>",                                                                       desc  ="files"},
+    {"<leader>fb",      "<cmd>FzfLua buffers<cr>",                                                                     desc  ="buffers"},
+    {"<leader>fc",      "<cmd>FzfLua git_commits<cr>",                                                                 desc  ="commits"},
+    {"<leader>fg",      "<cmd>FzfLua git_files<cr>",                                                                   desc  ="git files"},
+    {"<leader>ff",      "<cmd>FzfLua files<cr>",                                                                       desc  ="files"},
+    {"<leader>fp",      "<cmd>FzfLua files             cwd=" ..    require("lazy.core.config").options.root .. "<cr>", desc ="files"},
+    {"<leader>sh",      "<cmd>FzfLua helptags<cr>",                                                                    desc  =     "help"},
+    {"<leader>sk",      "<cmd>FzfLua keymaps<cr>",                                                                     desc  ="keymaps"},
     -- stylua: ignore end
   },
   opts = {
