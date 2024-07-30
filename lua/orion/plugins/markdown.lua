9835e88c2352fb7b945e24839d98ccd73ff75d2d
Dhruvin Shah <dhruvin3@gmail.com>
2024-07-30 08:22:02 -0400

    feat(plugins): adding markdown.nvim

---
+++ lua/orion/plugins/markdown.lua
@@ -0,0 +1,8 @@
+return {
+  "MeanderingProgrammer/markdown.nvim",
+  ft = "markdown",
+  main = "render-markdown",
+  opts = {},
+  name = "render-markdown",
+  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
+}
