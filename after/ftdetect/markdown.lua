eeb3947606c1a86186d6ce57722ef4084511cdcd
Dhruvin Shah <dshah@evertz.com>
2024-08-06 11:22:09 -0400

    feat(plugins): better markdown eco system
    * added markdown-preview
    * added Glow for markdown preview

---
+++ after/ftdetect/markdown.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader>cp", "<cmd>MarkdownPreviewToggle<cr>", { desc = "preview", silent = true })
+vim.keymap.set("n", "<leader>cP", "<cmd>Glow<cr>", { desc = "preview", silent = true })
