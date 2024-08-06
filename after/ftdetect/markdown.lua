255f496c3bc72423ea21176cfc5290a6b16e8ce7
Dhruvin Shah <dhruvin3@gmail.com>
2024-08-06 15:21:53 -0400

    feat(config): adding spell check
    - markdown
    - gitcommit

--- after/ftdetect/markdown.lua
+++ after/ftdetect/markdown.lua
@@ -1,2 +1,6 @@
-vim.keymap.set("n", "<leader>cp", "<cmd>MarkdownPreviewToggle<cr>", { desc = "preview", silent = true })
-vim.keymap.set("n", "<leader>cP", "<cmd>Glow<cr>", { desc = "preview", silent = true })
+-- options
+vim.opt_local.spell = true
+
+-- keymaps
+vim.keymap.set("n", "<leader>cp", "<cmd>MarkdownPreviewToggle<cr>", { desc = "preview" })
+vim.keymap.set("n", "<leader>cP", "<cmd>Glow<cr>", { desc = "preview" })
