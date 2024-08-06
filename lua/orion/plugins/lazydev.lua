471f88c5c1a5e155cadf1c30d0649f8f43d4ba40
Dhruvin Shah <dhruvin3@gmail.com>
2024-08-05 20:37:10 -0400

    chore(code): auto formatting

--- lua/orion/plugins/lazydev.lua
+++ lua/orion/plugins/lazydev.lua
@@ -3,7 +3,7 @@ return {
     "folke/lazydev.nvim",
     ft = "lua",
     cmd = "LazyDev",
-    dependencies = { "Bilal2453/luvit-meta"},
+    dependencies = { "Bilal2453/luvit-meta" },
     opts = {
       library = {
         { path = "luvit-meta/library", words = { "vim%.uv" } },
@@ -13,7 +13,7 @@ return {
 
   {
     "Bilal2453/luvit-meta",
-    lazy = true
+    lazy = true,
   },
 
   {
