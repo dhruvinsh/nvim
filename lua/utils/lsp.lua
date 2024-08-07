38cad8d6da26d1e3752426ac213addd03e8df103
Dhruvin Shah <dshah@evertz.com>
2024-08-07 14:49:33 -0400

    feat(lsp): better lua lsp config

--- lua/utils/lsp.lua
+++ lua/utils/lsp.lua
@@ -14,24 +14,27 @@ M.servers = {
   lua_ls = {
     on_init = function(client)
       local path = client.workspace_folders and client.workspace_folders[1] and client.workspace_folders[1].name
-      if not path or not (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc")) then
-        client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
-          Lua = {
-            runtime = {
-              version = "LuaJIT",
-            },
-            workspace = {
-              checkThirdParty = false,
-              library = {
-                vim.env.VIMRUNTIME,
-                "${3rd}/luv/library",
-              },
+
+      -- give .luarc.json higher priority
+      if path and vim.uv.fs_stat(path .. "/.luarc.json") then
+        return
+      end
+
+      client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
+        Lua = {
+          runtime = {
+            version = "LuaJIT",
+          },
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME,
+              "${3rd}/luv/library",
             },
           },
-        })
-        client.notify(vim.lsp.protocol.Methods.workspace_didChangeConfiguration, { settings = client.config.settings })
-      end
-      return true
+        },
+      })
+      client.notify(vim.lsp.protocol.Methods.workspace_didChangeConfiguration, { settings = client.config.settings })
     end,
     settings = {
       Lua = {
