902770ab4362a8353b093a7bf5b91106e417a673
Dhruvin Shah <dshah@evertz.com>
2024-08-09 16:46:21 -0400

    feat(plugins): adding wezterm types for lazydev

--- lua/orion/plugins/lazydev.lua
+++ lua/orion/plugins/lazydev.lua
@@ -7,11 +7,13 @@ return {
     opts = {
       library = {
         { path = "luvit-meta/library", words = { "vim%.uv" } },
+        { path = "wezterm-types", words = { "wezterm" } },
       },
     },
   },
 
   { "Bilal2453/luvit-meta", lazy = true },
+  { "justinsgithub/wezterm-types", lazy = true },
 
   {
     "hrsh7th/nvim-cmp",
