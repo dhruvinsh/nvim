43a3d769a6d7c34b8d60023a5ab83900f45c369e
Dhruvin Shah <dhruvin3@gmail.com>
2024-08-13 17:23:49 -0400

    feat(overseer): better job runner and keymaps
    * job revolves around creating build.sh jobs and run it.
    * few options added,
      run build.sh
      run build.sh with args
      run last job

--- lua/orion/plugins/overseer.lua
+++ lua/orion/plugins/overseer.lua
@@ -1,11 +1,42 @@
 return {
   "stevearc/overseer.nvim",
-  cmds = { "OverseerRun", "OverseerToggle" },
+  cmd = { "Build", "OverseerClose", "OverseerOpen", "OverseerRun", "OverseerToggle" },
   opts = {
     templates = { "builtin" },
+    task_list = {
+      default_detail = 2,
+      direction = "bottom",
+      max_width = { 600, 0.7 },
+    },
   },
   keys = {
-    { "<leader>cb", "<cmd>Build<cr>", desc = "build" },
+    { "<leader>jb", "<cmd>Build<cr>", desc = "build" },
+    {
+      "<leader>ji",
+      function()
+        vim.ui.input({ prompt = "Enter args: " }, function(args)
+          if args == nil then
+            return
+          end
+          vim.cmd("Build " .. args)
+        end)
+      end,
+      desc = "interective",
+    },
+    {
+      "<leader>jr",
+      function()
+        local overseer = require("overseer")
+        local tasks = overseer.list_tasks({ recent_first = true })
+
+        if vim.tbl_isempty(tasks) then
+          vim.notify("No task found", vim.log.levels.WARN)
+        else
+          overseer.run_action(tasks[1], "restart")
+        end
+      end,
+      desc = "recent job",
+    },
   },
   config = function(_, opts)
     local overseer = require("overseer")
