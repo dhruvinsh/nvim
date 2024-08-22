1734634a5560426a9abebe9f46ef7100a09228d8
Dhruvin Shah <dshah@evertz.com>
2024-08-22 13:05:07 -0400

    feat(utils): toggle functions added

---
+++ lua/utils/toggle.lua
@@ -0,0 +1,44 @@
+local M = {}
+M.__index = M
+
+function M:new(name, action)
+  local obj = {
+    name = name,
+    state = true, -- always assume that state is true.
+    action = action,
+  }
+  setmetatable(obj, M)
+  return obj
+end
+
+function M:toggle()
+  self.state = not self.state
+  self.action(self.state)
+end
+
+--
+-- Collection of the toggles
+--
+local H = {}
+
+H.diagnostic = M:new("diagnostic", function(state)
+  vim.diagnostic.enable(state)
+end)
+
+H.format_local = M:new("format (local)", function(state)
+  if state then
+    vim.cmd("FormatDisable!")
+  else
+    vim.cmd("FormatEnable")
+  end
+end)
+
+H.format_global = M:new("format (global)", function(state)
+  if state then
+    vim.cmd("FormatDisable")
+  else
+    vim.cmd("FormatEnable")
+  end
+end)
+
+return H
