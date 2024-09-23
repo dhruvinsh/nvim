e1d7e1f5401efadce1d99787b798067985afb45f
Dhruvin Shah <dhruvin3@gmail.com>
2024-09-23 12:55:42 -0400

    feat: adding codecompanion component

--- lua/orion/plugins/codecompanion.lua
+++ lua/orion/plugins/codecompanion.lua
@@ -8,45 +8,58 @@ return {
     "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
   },
-  opts = {
-    strategies = {
-      chat = {
-        adapter = "copilot",
-      },
-      inline = {
-        adapter = "copilot",
-      },
-      agent = {
-        adapter = "copilot",
-      },
-    },
-    display = { chat = { show_settings = true } },
-    pre_defined_prompts = {
-      ["Generate a Commit Message for Staged Files"] = {
-        strategy = "chat",
-        description = "staged file commit messages",
-        opts = {
-          index = 9,
-          default_prompt = true,
-          mapping = "<LocalLeader>gm",
-          slash_cmd = "commit",
-          auto_submit = true,
+  opts = function()
+    local component = require("util.codecompanion")
+    do
+      local ok, lualine = pcall(require, "lualine")
+      if not ok then
+        return
+      end
+      local lualine_cfg = lualine.get_config()
+      table.insert(lualine_cfg.sections.lualine_x, 1, component)
+      lualine.setup(lualine_cfg)
+    end
+
+    return {
+      strategies = {
+        chat = {
+          adapter = "copilot",
         },
-        prompts = {
-          {
-            role = "user",
-            contains_code = true,
-            content = function()
-              return "You are an expert at following the Conventional Commit specification. Given the git diff listed below, please generate a commit message for me:"
-                .. "\n\n```\n"
-                .. vim.fn.system("git diff --staged")
-                .. "\n```"
-            end,
+        inline = {
+          adapter = "copilot",
+        },
+        agent = {
+          adapter = "copilot",
+        },
+      },
+      display = { chat = { show_settings = true } },
+      pre_defined_prompts = {
+        ["Generate a Commit Message for Staged Files"] = {
+          strategy = "chat",
+          description = "staged file commit messages",
+          opts = {
+            index = 9,
+            default_prompt = true,
+            mapping = "<LocalLeader>gm",
+            slash_cmd = "commit",
+            auto_submit = true,
+          },
+          prompts = {
+            {
+              role = "user",
+              contains_code = true,
+              content = function()
+                return "You are an expert at following the Conventional Commit specification. Given the git diff listed below, please generate a commit message for me:"
+                  .. "\n\n```\n"
+                  .. vim.fn.system("git diff --staged")
+                  .. "\n```"
+              end,
+            },
           },
         },
       },
-    },
-  },
+    }
+  end,
   keys = {
     { "<leader>aa", "<cmd>CodeCompanionActions<cr>", desc = "action", mode = { "n", "v" } },
     { "<leader>at", "<cmd>CodeCompanionToggle<cr>", desc = "toggle", mode = { "n", "v" } },
