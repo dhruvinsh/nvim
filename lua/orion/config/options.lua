19846b6740968e0b2ae81ca2799f41473fd89844
Dhruvin Shah <dhruvin3@gmail.com>
2024-09-24 13:37:47 -0400

    chore: spell correction

--- lua/orion/config/options.lua
+++ lua/orion/config/options.lua
@@ -91,7 +91,7 @@ if utils.is_win then
   if vim.fn.executable("python") then
     vim.g.python3_host_prog = "python"
   end
--- on linux and mac os I use pyenv to manage python, soon to be `Rye`
+-- on Linux and MacOS I use pyenv to manage python, soon to be `Rye`
 elseif vim.fn.executable("pyenv") then
   local py_version = vim.fn.trim(vim.fn.system("pyenv global"))
   vim.g.python3_host_prog = "~/.pyenv/versions/" .. py_version .. "/bin/python3"
