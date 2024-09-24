e1be8c2169d06a7fc9fcf8cd01a3d090e457ee29
Dhruvin Shah <dhruvin3@gmail.com>
2024-09-24 13:29:49 -0400

    feat(colors): adding some overrides for gruvbox

--- lua/orion/plugins/colors.lua
+++ lua/orion/plugins/colors.lua
@@ -11,6 +11,16 @@ return {
       italic = {
         strings = false,
       },
+      overrides = {
+        SignColumn = { link = "Normal" },
+        GruvboxGreenSign = { bg = "" },
+        GruvboxOrangeSign = { bg = "" },
+        GruvboxPurpleSign = { bg = "" },
+        GruvboxYellowSign = { bg = "" },
+        GruvboxRedSign = { bg = "" },
+        GruvboxBlueSign = { bg = "" },
+        GruvboxAquaSign = { bg = "" },
+      },
     },
   },
 
