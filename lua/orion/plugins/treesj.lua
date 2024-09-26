1537ab728ac75ca2c22c792c34a9c1e07f47f2ae
Dhruvin Shah <dhruvin3@gmail.com>
2024-09-25 20:37:48 -0400

    feat(plugin): config improvement to treesj

--- lua/orion/plugins/treesj.lua
+++ lua/orion/plugins/treesj.lua
@@ -1,7 +1,11 @@
 return {
   "Wansmer/treesj",
+  cmd = { "TSJToggle", "TSJSplit", "TSJJoin" },
+  opts = {
+    use_default_keymaps = false,
+    max_join_length = 150,
+  },
   keys = {
     { "gJ", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
   },
-  opts = { use_default_keymaps = false, max_join_length = 150 },
 }
