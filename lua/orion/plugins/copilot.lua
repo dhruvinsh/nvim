313dec74ead0f509f04f3f1fce762f82d2d77cfa
Dhruvin Shah <dhruvin3@gmail.com>
2024-12-17 23:20:56 -0500

    fix(copilot): making completion working

--- lua/orion/plugins/copilot.lua
+++ lua/orion/plugins/copilot.lua
@@ -28,14 +28,16 @@ return {
     "saghen/blink.cmp",
     dependencies = { "giuxtaposition/blink-cmp-copilot" },
     opts = {
-      default = { "copilot" },
-      providers = {
-        copilot = {
-          name = "copilot",
-          module = "blink-cmp-copilot",
-          kind = "Copilot",
-          score_offset = 100,
-          async = true,
+      sources = {
+        default = { "copilot" },
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-cmp-copilot",
+            kind = "Copilot",
+            score_offset = 100,
+            async = true,
+          },
         },
       },
     },
