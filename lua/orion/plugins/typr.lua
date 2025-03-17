65e9f2a7aeeec98bdc23784333b8e0826f22f15c
Dhruvin Shah <dhruvin3@gmail.com>
2025-03-17 10:43:43 -0400

    feat(plugins): add typr plugin with keybindings

---
+++ lua/orion/plugins/typr.lua
@@ -0,0 +1,10 @@
+return {
+  "nvzone/typr",
+  cmd = { "Typr", "TyprStats" },
+  dependencies = "nvzone/volt",
+  keys = {
+    { "<leader>kt", "<cmd>Typr<cr>", desc = "Typer" },
+    { "<leader>ks", "<cmd>TyprStats<cr>", desc = "Stats" },
+  },
+  opts = {},
+}
