e7da64dc6378f4527c3a700690917d8637e18ffb
Dhruvin Shah <dhruvin3@gmail.com>
2025-04-19 21:24:42 -0400

    feat(plugins): add auto-cmdheight dependency to vim-fugitive

--- lua/orion/plugins/git.lua
+++ lua/orion/plugins/git.lua
@@ -1,6 +1,7 @@
 return {
   {
     "tpope/vim-fugitive",
+    dependencies = { "jake-stewart/auto-cmdheight.nvim", lazy = true },
     cmd = "Git",
     keys = function()
       local toggle_fugitive = function()
