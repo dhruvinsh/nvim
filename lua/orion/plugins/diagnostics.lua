222651f55ce5420400c77f8f72373bfdb84aa257
Dhruvin Shah <dhruvin3@gmail.com>
2025-04-21 11:42:06 -0400

    feat(diagnostics): add keybinding to toggle virtual diagnostic lines
    Add 'gK' keybinding to toggle the visibility of virtual diagnostic lines.
    Clean up unnecessary comments in the configuration.

--- lua/orion/plugins/diagnostics.lua
+++ lua/orion/plugins/diagnostics.lua
@@ -1,9 +1,19 @@
 return {
   "rachartier/tiny-inline-diagnostic.nvim",
-  event = "LspAttach", -- Or `LspAttach`
-  priority = 1000, -- needs to be loaded in first
+  event = "LspAttach",
+  priority = 1000,
+  keys = {
+    {
+      "gK",
+      function()
+        local new_config = not vim.diagnostic.config().virtual_lines
+        vim.diagnostic.config({ virtual_lines = new_config })
+      end,
+      desc = "Toggle diagnostic virtual_lines",
+    },
+  },
   config = function()
     require("tiny-inline-diagnostic").setup()
-    vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
+    vim.diagnostic.config({ virtual_text = false })
   end,
 }
