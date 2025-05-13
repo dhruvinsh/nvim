22132f1d2232244226fef34e5a882281254520c6
Dhruvin Shah <dhruvin3@gmail.com>
2025-05-13 17:41:28 -0400

    feat(util): enhance toggle module with customizable initial state
    - Added a `state` parameter to the `M:new` function, allowing customizable initial states for toggles.
    - Updated all toggle instances to specify their initial states explicitly.
    - Improved logic for toggles like `format_local` and `format_global` to handle state transitions more clearly.
    - Adjusted `colorscheme` and `wrap` toggles to derive their initial states dynamically from Vim options.

--- lua/util/toggle.lua
+++ lua/util/toggle.lua
@@ -1,10 +1,10 @@
 local M = {}
 M.__index = M
 
-function M:new(name, action)
+function M:new(name, state, action)
   local obj = {
     name = name,
-    state = true, -- always assume that state is true.
+    state = state,
     action = action,
   }
   setmetatable(obj, M)
@@ -21,42 +21,42 @@ end
 --
 local H = {}
 
-H.diagnostic = M:new("diagnostic", function(state)
+H.diagnostic = M:new("diagnostic", true, function(state)
   vim.diagnostic.enable(state)
 end)
 
-H.format_local = M:new("format (local)", function(state)
+H.format_local = M:new("format (local)", true, function(state)
   if state then
-    vim.cmd("FormatDisable!")
-    vim.notify("Local format is disabled", vim.log.levels.INFO)
-  else
     vim.cmd("FormatEnable")
     vim.notify("Local format is enabled", vim.log.levels.INFO)
+  else
+    vim.cmd("FormatDisable!")
+    vim.notify("Local format is disabled", vim.log.levels.INFO)
   end
 end)
 
-H.format_global = M:new("format (global)", function(state)
+H.format_global = M:new("format (global)", true, function(state)
   if state then
-    vim.cmd("FormatDisable")
-    vim.notify("Global format is disabled", vim.log.levels.INFO)
-  else
     vim.cmd("FormatEnable")
     vim.notify("Global format is enabled", vim.log.levels.INFO)
+  else
+    vim.cmd("FormatDisable")
+    vim.notify("Global format is disabled", vim.log.levels.INFO)
   end
 end)
 
-H.inlay_hint = M:new("inlay hint", function(state)
+H.inlay_hint = M:new("inlay hint", true, function(state)
   vim.lsp.inlay_hint.enable(state)
 end)
 
 -- toggle vim.o.background "dark" or "light"
-H.colorscheme = M:new("colorscheme", function(state)
+H.colorscheme = M:new("colorscheme", vim.o.background == "dark", function(state)
   vim.o.background = state and "dark" or "light"
   vim.cmd.colorscheme(vim.g.colorscheme)
 end)
 
 -- toggle wrap
-H.wrap = M:new("wrap", function(state)
+H.wrap = M:new("wrap", vim.o.wrap, function(state)
   vim.o.wrap = state
 end)
 
