016cb6dc7546250c3b65ac92239f023ff9b57033
Dhruvin Shah <dhruvin3@gmail.com>
2025-05-29 10:54:28 -0400

    feat(git): add mini.diff plugin for enhanced diff functionality
    Added the "echasnovski/mini.diff" plugin to improve inline and overall diff capabilities.
    Configured the plugin with default settings, disabling the source by default.

--- lua/orion/plugins/git.lua
+++ lua/orion/plugins/git.lua
@@ -79,4 +79,15 @@ return {
       end,
     },
   },
+
+  {
+    "echasnovski/mini.diff", -- Inline and better diff over the default
+    config = function()
+      local diff = require("mini.diff")
+      diff.setup({
+        -- Disabled by default
+        source = diff.gen_source.none(),
+      })
+    end,
+  },
 }
