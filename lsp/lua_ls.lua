1f78e9568bf7727ff7659b1d3c914aa55962d4e8
Dhruvin Shah <dhruvin3@gmail.com>
2025-05-30 11:55:00 -0400

    feat(plugins): bring back lazydev.nvim
    Added lazydev.nvim plugin with Lua filetype support and custom library configuration.
    Integrated blink.cmp with lazydev as a source and provider, including specific module and scoring adjustments.

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -1,10 +1,3 @@
----@param name string plugin name
----@param trailing_path string? optional path to append to the plugin's lua directory
-local stdpath = function(name, trailing_path)
-  trailing_path = trailing_path or "/lua"
-  return vim.fn.stdpath("data") .. "/lazy/" .. name .. trailing_path
-end
-
 ---@type vim.lsp.Config
 return {
   cmd = { "lua-language-server" },
@@ -22,32 +15,6 @@ return {
         library = {
           vim.env.VIMRUNTIME,
           "${3rd}/luv/library",
-          -- Runtime and builtin libraries.
-          "$VIMRUNTIME/lua",
-          "${3rd}/luv/library",
-          -- Extra configuration helpers.
-          "?.lua",
-          "?/?.lua",
-          -- Include some plugins for API completion.
-          stdpath("blink.cmp"),
-          stdpath("bufferline.nvim"),
-          stdpath("catppuccin"),
-          stdpath("codecompanion.nvim"),
-          stdpath("conform.nvim"),
-          stdpath("fastaction.nvim"),
-          stdpath("fzf-lua"),
-          stdpath("gitsigns.nvim"),
-          stdpath("grug-far.nvim"),
-          stdpath("lazy.nvim"),
-          stdpath("mcphub.nvim"),
-          stdpath("mini.ai"),
-          stdpath("mini.bufremove"),
-          stdpath("mini.files"),
-          stdpath("nvim-autopairs"),
-          stdpath("nvim-treesitter-context"),
-          stdpath("overseer.nvim", "/lua/overseer/"),
-          stdpath("snacks.nvim"),
-          stdpath("toggleterm.nvim"),
         },
       },
       telemetry = {
---
+++ lua/orion/plugins/lazydev.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+
+  {
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        default = { "lazydev" },
+        providers = {
+          lsp = { fallbacks = { "lazydev" } },
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            score_offset = 100,
+          },
+        },
+      },
+    },
+  },
+}
