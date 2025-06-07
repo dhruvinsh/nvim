c567c9d929dfab333709f3de55b0a5fa4f0d2818
Dhruvin Shah <dhruvin3@gmail.com>
2025-06-06 23:29:23 -0400

    feat(grug-far): update plugin configuration
    Updated the `grug-far.nvim` plugin to the latest commit `c36f5f9`. Enhanced configuration by adding new options for `helpLine`, `helpWindow`, and input display settings. This improves usability and customization.

--- lua/orion/plugins/grug.lua
+++ lua/orion/plugins/grug.lua
@@ -4,10 +4,20 @@ return {
   ---@module "grug-far"
   ---@type grug.far.OptionsOverride
   opts = {
-    transient = true,
+    helpLine = {
+      enabled = false,
+    },
+    helpWindow = {
+      relative = "editor",
+      width = math.ceil(vim.o.columns * 0.9),
+    },
     keymaps = {
       help = { n = "?" },
     },
+    showCompactInputs = true,
+    showInputsBottomPadding = false,
+    showInputsTopPadding = false,
+    transient = true,
   },
   keys = {
     { "<leader>cs", "<cmd>GrugFar ripgrep<cr>", desc = "search (ripgrep)", mode = { "n", "v" } },
