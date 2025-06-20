3ff81bc2c47aaf69c7ffab5539dd994bc20f7635
Dhruvin Shah <dhruvin3@gmail.com>
2025-06-20 16:16:49 -0400

    refactor(oil): move oil.nvim plugin config to separate file
    Relocated the configuration for stevearc/oil.nvim from file.lua to a new dedicated oil.lua file for better organization and maintainability.

---
+++ lua/orion/plugins/oil.lua
@@ -0,0 +1,10 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "echasnovski/mini.icons" },
+  lazy = false,
+  cmd = { "Oil" },
+  opts = {},
+  keys = {
+    { "-", "<cmd>Oil<cr>", desc = "oil" },
+  },
+}
