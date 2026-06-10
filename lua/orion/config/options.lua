0f7da8898c612b04e3c5eb2e6341013d43c734a0
Dhruvin Shah <dhruvin3@gmail.com>
2026-06-10 09:45:39 -0400

    feat(options): treat dash as word and update fold character
    Set iskeyword to include dash so it is treated as part of a word.
    Change fold fill character to a dashed line for improved visibility.

--- lua/orion/config/options.lua
+++ lua/orion/config/options.lua
@@ -22,13 +22,14 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
 vim.opt.tabstop = 2
+vim.opt.iskeyword = "@,48-57,_,192-255,-" -- Treat dash as `word` textobject part
 -- NOTE: something here breaks the lsp "K" mappings upon restore
 -- vim.opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
 -- folds
 vim.opt.fillchars = {
   diff = "╱",
-  fold = " ",
+  fold = "┄",
   foldclose = "",
   foldopen = "",
   eob = " ",
