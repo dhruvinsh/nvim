dbfd03da61365b53431189a548d5e431155c31ec
Dhruvin Shah <dhruvin3@gmail.com>
2026-06-10 08:40:59 -0400

    feat(treesitter): add is-mise? predicate for TOML files
    Introduce a custom predicate to detect mise-related TOML files
    using filename pattern matching.

--- lua/orion/plugins/treesitter.lua
+++ lua/orion/plugins/treesitter.lua
@@ -82,4 +82,11 @@ return {
   config = function(_, opts)
     require("nvim-treesitter").setup(opts)
   end,
+  init = function()
+    require("vim.treesitter.query").add_predicate("is-mise?", function(_, _, bufnr, _)
+      local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
+      local filename = vim.fn.fnamemodify(filepath, ":t")
+      return string.match(filename, ".*mise.*%.toml$") ~= nil
+    end, { force = true, all = false })
+  end,
 }
