03c397d72c0082cf82fef68ddd7a4ccd94d35dae
Dhruvin Shah <dhruvin3@gmail.com>
2026-06-16 16:44:50 -0400

    refactor(markdown): remove custom folding and indentation settings

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -4,10 +4,3 @@ vim.opt_local.spell = true
 -- keymaps
 vim.keymap.set("n", "<leader>cp", "<cmd>MarkdownPreviewToggle<cr>", { desc = "preview", buffer = 0 })
 vim.keymap.set("n", "<leader>cP", "<cmd>Glow<cr>", { desc = "preview", buffer = 0 })
-
--- folding
-vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.wo[0][0].foldmethod = "expr"
-
--- indentation
-vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
