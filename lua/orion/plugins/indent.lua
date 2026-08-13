b25ab645ea49dbd7ae89598deeb4069d55d79f96
Dhruvin Shah <dhruvin3@gmail.com>
2026-08-13 17:02:04 -0400

    chore(indent): lazy-load blink.indent on InsertEnter
    Add blink.cmp as a dependency and bump blink.indent to the latest
    commit.

--- lua/orion/plugins/indent.lua
+++ lua/orion/plugins/indent.lua
@@ -1,5 +1,7 @@
 return {
   "saghen/blink.indent",
+  event = { "InsertEnter" },
+  dependencies = { "saghen/blink.cmp" },
   --- @module 'blink.indent'
   --- @type blink.indent.Config
   -- opts = {},
